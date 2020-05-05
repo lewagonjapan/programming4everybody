e8678d3a89dc20cb34663b8f9844c805896f5d79
Sylvain Pierre <sylvain.pierre@officience.com>
2020-05-05 18:47:55 +0900

    Added slides for session 5

--- 00_livecode.rb
+++ 00_livecode.rb
@@ -3,31 +3,6 @@
 
 
 
-# Default Parameter
-# If I pass 1 argument, rev will be false
-# If I pass 2 arguments, rev will take the value of
-# my second argument
-def alphabetize(arr, rev = false)
-  if rev
-    arr.sort!.reverse
-  else
-    arr.sort!
-  end
-end
-
-numbers = [5, 1, 3, 8]
-
-# Calling alphabetize with 1 argument
-# rev = false
-p alphabetize(numbers)
-
-# Calling alphabetize with 2 arguments
-# rev = true
-p alphabetize(numbers, true)
-
-
-
-
 
 
 
