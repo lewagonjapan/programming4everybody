dd834255a59aa04591320c59b6224255744b6949
Sylvain Pierre <sylvain.pierre@officience.com>
2021-05-18 22:05:28 +0900

    Added Slides and Livecode for session 3

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,196 @@
 
-### Arrays
+# ======= RECAP =======
+
+
+# So far, we saw FOUR core programming notions
+# - Data Types
+# - Variables
+# - Conditionals
+# - Looping
+
+# Three more notions for tonight
+# - Arrays (the simplest 'database')
+# - Iterators & blocks
+# - Methods
+
+# We're not talking about Hashes!
+
+
+# ======= ARRAYS =======
+
+[] # the most basic array, the empty array
+[].class # => Array
+
+# Arrays are a collection of objects
+# Usually, they are all the same type
+
+[1, 2, 3] # Array of integers
+['sasha', 'sylvain', 'douglas'] # array of strings
+[[1, 2], [1, 2]] # Array of 2 arrays
+
+# In Ruby, we can also do that:
+[1, 'sasha', [1, 2]] # array of 3 'random' elements
+
+
+# CRUD for arrays
+
+# The most important thing with array: CRUD
+# CRUD stands for Create, Read, Update, Delete
+
+le_wagon_team = ['Sasha', 'Yann', 'Douglas']
+# index             0        1        2
+
+# how do we count with arrays?
+# With reference with an index
+# ⚠️ In an array, index starts at ZERO
+
+
+## Read
+
+# Theory: array[index]
+le_wagon_team[1] # => 'Yann'
+le_wagon_team[0..1] # => ['Sasha', 'Yann']
+le_wagon_team[-1] # => 'Douglas'
+
+# advanced read
+le_wagon_team.first # first element
+le_wagon_team.last # last element
+
+
+## Create
+# Theory: array.push(element_to_add)
+le_wagon_team.push('Devin')
+
+# Another way (more common): array << element_to_add
+le_wagon_team << 'Ada'
+
+
+## Update
+
+# Replacing an element by another
+# Theory: Read an element, and assign a new value
+# array[index] = new_value
+
+le_wagon_team[3] = 'Dave'
+
+
+## Delete
+
+# Two ways
+# .delete(element_to_delete)
+# .delete deletes ALL instances of that object
+
+le_wagon_team.delete('Ada') # => Deletes all instance of 'Ada'
+
+# .delete_at(index) # ⚠️ parenthesis, not []
+le_wagon_team.delete_at(3)
+
+
+
+# ======= ITERATORS =======
+
+
+# Iterators are a way to 'iterate' over arrays
+
+# most basic iterator that you already know
+
+# .each
+
+radiohead = ['Phil', 'Thom', 'Jonny', 'Ed', 'Colin']
+
+radiohead.each do |band_member|
+  puts "#{band_member} plays with Radiohead"
+end
+
+
+## Advanced iterators
+
+# .each_with_index
+
+radiohead.each_with_index do |member, index|
+  puts "#{index + 1}) #{member} plays with Radiohead"
+end
+
+
+# .map
+
+# allows to replace all elements in an array following a specific pattern
+
+downcased_radiohead = radiohead.map do |member|
+  # transformation
+  member.downcase
+end
+
+# The original array is not modified
+p radiohead # => ['Phil', 'Thom', 'Jonny', 'Ed', 'Colin']
+
+# .map returns an array of the same size, with the transformation specified
+downcased_radiohead # => ['phil', 'thom', 'jonny', 'ed', 'colin']
+
+
+# map is called on an array
+# we specify a 'transformation'
+# .map returns an array of the same size, 'transformed'
+
+
+# ['Phil', 'Thom', 'Jonny', 'Ed', 'Colin']
+#    ||       ||      ||     ||      ||
+#         transformation (downcase)
+#    ||       ||      ||     ||      ||
+# ['phil', 'thom', 'jonny', 'ed', 'colin']
+
+
+# .select
+
+# select elements in an array based on a CONDITION
+# specify a condition when iterating
+
+five_letter_names = radiohead.select do |member|
+  # ⚠️ Condition
+  member.length == 5
+end
+
+p radiohead
+p five_letter_names # => ['Jonny', 'Colin']
+
+# select is called on an array, and returns a (smaller) array of selected elements
+
+
+# select is the basis behind search in web apps
+# For example: Looking for apartments in Tokyo on Airbnb
+
+
+# ======= ITERATORS =======
+
+radiohead = ['Phil', 'Thom', 'Jonny', 'Ed', 'Colin']
+
+radiohead.each do |band_member|
+  puts "#{band_member} plays with Radiohead"
+end
+
+# do.... end is called a BLOCK
+# |band_member| is called a block parameter
+# a block is a way to pass 'some code' to a method (in that case, the .each method)
+
+# DISCLAIMER
+# Blocks are advanced concepts, you don't need to know much about them right now
+
+# One-line block syntax for blocks:
+
+radiohead.select { |member| member.length == 5 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,145 @@
 
 ### Methods
+
+
+
+# ======= METHODS =======
+
+# Why do we need methods?
+
+# Developers are LAZY
+# They don't want to repeat themselves
+# Methods are a way to do that
+# A method allows us to store some code, and re-use it later
+
+# a method to compute the full_name,
+# given the first and last name
+
+# Defining a method
+def full_name(first, last)
+  return first.capitalize + ' ' + last.capitalize
+end
+
+
+# Calling a method
+puts full_name('sylvain', 'pierre')
+
+puts full_name('john', 'doe')
+
+# Calling a method and passing variables as arguments:
+
+puts 'Hey, what is your first name?'
+first_name = gets.chomp
+
+puts 'Hey, what is your last name?'
+last_name = gets.chomp
+
+puts full_name(first_name, last_name)
+
+
+# Three important concepts about Methods:
+
+# 1) Difference between DEFINING and CALLING a method
+
+# 2) Method Signature:
+# - Name of the method
+# - Number (and types) of parameters
+# - what does it return
+
+# 3) Conventions:
+# - name should be lower_snake_case
+# - ? methods should return true or false
+# - ! methods modify the original input
+
+
+
+
+# ======= LIVECODE =======
+
+# Scrape the Web 😱😱
+
+# Scrape prices from Rakuten
+# DISCLAIMER ⚠️ This is a bit advanced
+
+
+# I need to be able to open a web page
+require 'open-uri'
+
+# I need to be able to parse some HTML
+require 'nokogiri'
+
+# Asking the user what prices they want to find:
+puts 'Hey! What do you want to look for on Rakuten?'
+user_input = gets.chomp
+
+# opening a link, and 'reading' it, interpolating the user answer:
+doc = open("https://search.rakuten.co.jp/search/mall/#{user_input}/").read
+
+# doc is just a HUGE string
+
+# Parsing the big string into something that I can easily read:
+html = Nokogiri::HTML(doc)
+
+
+prices = html.search('.price .important').first(10).map do |element|
+  element.text.strip
+end
+
+p prices # => displays the prices for the first 10 search results from Rakuten
+
+
+
+# ======= Q&A =======
+
+
+# 1️⃣ - Can we store a variable in an array?
+# Yes!
+
+le_wagon_team = ['Sasha', 'Douglas', 'Yann']
+
+first_name = 'Ada'
+le_wagon_team << first_name
+p le_wagon_team # => ['Sasha', 'Douglas', 'Yann', 'Ada']
+
+
+
+# 2️⃣ - How can we mirror the behavior of .map with .each?
+
+upcased_members = []
+
+radiohead.each do |member|
+  upcased_members << member.upcase
+end
+
+p radiohead
+p upcased_members
+
+
+
+# 3️⃣ - Where can I find the list of gems available?
+# https://rubygems.org/
+
+
+
+# 4️⃣ - What are some popular Ruby gems?
+
+# Rspec - to write tests
+# Rubocop - A Linter (checking )
+# byebug - debugging gem
+# csv - open / read / write in csv files
+
+
+# 5️⃣ - Why not use puts in methods?
+# You need to keep flexibility for the methods you define
+
+def full_name(first, last)
+  first.capitalize! + ' ' + last.capitalize!
+end
+
+# Best Practice:
+# A method should return something, never puts.
+
+puts full_name('pierre', 'sylvain')
+
+
+
