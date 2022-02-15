5c83e32c46cd777413844dc065f8d1f7b60e836a
Sylvain Pierre <sylvain.pierre@officience.com>
2022-02-15 22:44:27 +0900

    added slides and livecode for session 3

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,95 @@
 
 ### Arrays
+
+
+
+# Last week, we saw 4 core programming concepts
+# Data Types, Variables, Conditionals, Looping
+
+# Tonight, we will NOT cover HASHES
+
+# Still we're going to see quite a few important concepts:
+# Arrays, Iterators & Blocks, Methods
+
+# We will also have a super cool LIVECODE
+
+# ===== ARRAYS =====
+
+
+# Recap on Arrays
+[].class # Delimited by square brackets
+
+# Any value can be stored in an array
+[1, 2, 3] # Array of 3 integers
+['sylvain', 'sasha'] # Array of two strings
+[1, 'sasha', true] # this is ok in Ruby, but not really common
+[[1, 2], [3, 4]] # Array of 2 arrays, each of those arrays have 2 elements
+
+# Counting in arrays
+# In an array, we start counting from index ZERO
+['sasha', 'sylvain', 'douglas', 'yann']
+#   0        1            2        3
+
+
+### CRUD for arrays
+
+# Create, Read, Update and Delete
+# We often talk about CRUD operations for databases
+# An array is a simple form of database
+
+le_wagon_team = ['sasha', 'sylvain', 'douglas', 'yann']
+
+###  Read
+
+# reading an element from an array
+# Theory: array[index]
+le_wagon_team[2] # SQUARE BRACKETS
+
+# Read the last element
+le_wagon_team[-1]
+
+# Convenient methods to read
+le_wagon_team.first
+le_wagon_team.last
+
+le_wagon_team[0..2] # Returns a sub-array
+
+
+### Create
+
+# Adding an element to an array
+# Theory: array.push(element_to_add)
+# OR
+# array << element_to_add
+
+le_wagon_team.push('wilson')
+
+le_wagon_team << 'junsuke'
+
+
+### Update
+
+# Changing / updating one element in the array
+# Read and re-assign
+# array[index] = new_value
+
+le_wagon_team[0] = 'albin'
+
+le_wagon_team[-1] = 'alisa'
+
+
+### Delete
+
+# Removing an element from an array
+# Two ways to do that:
+# By element: array.delete(element_to_delete)
+# By index: array.delete_at(index_to_delete)
+
+# BE CAREFUL, .delete() and .delete_at() are METHODS
+# So we pass arguments with PARENTHESIS
+
+le_wagon_team.delete('alisa')
+p le_wagon_team
+
+le_wagon_team.delete_at(-1)
+p le_wagon_team
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,169 @@
 
 ### Methods
+
+
+# ===== ITERATORS & BLOCKS ===== 
+
+# What are iterators?
+# Iterators are methods to *iterate* (go over) an array and do something with each element
+
+### Simple iterator
+
+# .each
+
+radiohead = ['Thom', 'Ed', 'Colin', 'Jonny', 'Phil']
+
+radiohead.each do |member|
+    puts "#{member} plays with Radiohead"
+end
+
+# We can replicate the behavior of all the following interators using .each
+
+
+### Advanced Iterators
+
+# .each_with_index
+radiohead.each_with_index do |member, index|
+    puts "#{index + 1}) #{member} plays with Radiohead"
+end
+
+
+# .map
+
+# .map lets you apply a transformation to each element, and RETURNS an array of the same size, with each element replaced with that operation
+numbers = [1, 3, 5, 7]
+
+double_numbers = numbers.map do |number|
+    # Specify a transformation
+    number * 2
+end
+
+p double_numbers
+p numbers
+
+# numbers =  [1,   3,   5,   7]
+# === .map applies a transformation, in that case * 2
+# d_number = [2,   6,   10,  14]
+
+# for each iterator, remember:
+# What are they called on
+# What do we need to write in the block
+# What do they return
+
+
+# .select
+
+radiohead = ['Thom', 'Ed', 'Colin', 'Jonny', 'Todd']
+
+t_members = radiohead.select do |member|
+    # Specify a condition
+    member.chars.first == 'T' # Whenever this is true, will 'keep' the element
+end
+
+p t_members
+
+
+### What the h**l are blocks?
+# Blocks are a way to pass some code to methods
+
+radiohead.each do |member|
+    puts "#{member} plays with Radiohead"
+end
+
+# everything from 'do' to 'end' is called a block
+# 'member' is called a block parameter
+
+### 1-line block syntax
+
+# This:
+radiohead.select do |member|
+    member.chars.first == 'T'
+end
+
+# can be refactored with 1-line:
+radiohead.select { |member| member.chars.first == 'T' }
+
+# which one is better?
+# the answer is, as always, readability
+
+
+## ==== METHODS ====
+
+first_name = 'sasha'
+last_name = 'kaverina'
+
+puts "#{first_name.capitalize} #{last_name.capitalize}"
+
+# If we want to repeat this process, we need to write a lot of code:
+
+another_name = 'coelien'
+another_last_name = 'plain'
+
+puts "#{another_name.capitalize} #{another_last_name.capitalize}"
+
+# Methods are a way to avoid repeating yourself, and keep your code DRY
+# A method will let us store some code, to be able to re-use it later
+
+
+# Defining a method
+
+def full_name(first, last)
+    name = "#{first.capitalize} #{last.capitalize}"
+    return name
+end
+
+# Calling the method
+puts full_name('sasha', 'kaverina')
+
+# you can use puts in a method
+# but please, NEVER do it 🙏🙏🙏🙏
+
+
+# Methods work with variables too!
+puts 'What is your first name?'
+user_first = gets.chomp
+
+puts 'What is your last name?'
+user_last = gets.chomp
+
+capitalized_name = full_name(user_first, user_last)
+
+puts "Welcome to the class, #{capitalized_name}!"
+
+# Quick summary about methods
+# When using methods, remember their signature:
+# - What is the name of the method?
+# - Parameters? (type, how many)
+# - What does the method return?
+
+## Conventions
+# 1) method names are written in lower_snake_case
+# 2) methods that end with a ? will always return true or false
+# 3) methods that end with ! are called destructive or dangerous (DO NOT USE THEM)
+
+
+# ==== LIVECODE =====
+
+# Get prices from Rakuten AUTOMATICALLY
+# WARNING: this is advanced
+
+# a library to open a web page
+require 'open-uri'
+
+# a library that lets me parse (process) the HTML page that I got
+require 'nokogiri'
+
+puts 'Welcome to our Rakuten price scraper!'
+puts 'What would you like to look for today?'
+print '> '
+keyword = gets.chomp
+
+html = open("https://search.rakuten.co.jp/search/mall/#{keyword}/").read
+# html is actually a big String
+
+parsed_html = Nokogiri::HTML(html)
+
+# Look into the parsed HTML
+parsed_html.search('.price .important').first(20).each do |element|
+    puts element.text.strip
+end
