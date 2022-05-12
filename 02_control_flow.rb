43d106ff74df4da19ce5aad7fd836b6f01ed2c80
Sylvain Pierre <sylvain.pierre@officience.com>
2022-05-12 22:02:45 +0900

    Added Slides and Livecode for Session 2

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,112 @@
 ### Controling Ruby Flow
+
+
+# ===== RECAP =====
+
+### Data Types
+
+'Sylvain' # String
+"Sylvain" # Also a String
+# We cannot do String interpolation with single quotes
+
+5 # Integer
+5.4 # Float
+true # TrueClass
+false # FalseClass
+[1, 2, 3] # Array of 3 elements
+
+### Variables
+
+age = 25
+
+# The Integer 25 is assinged to the variable age
+# variables have to be written in lower_snake_case
+# = is called the assignment operator
+
+### Miscellaneous
+
+# Getting user input
+puts 'Hello, how old are you?'
+user_age = gets.chomp # Gets user input as a String
+
+# String Interpolation
+puts "Welcome, you will be #{user_age.to_i + 1} years old next year"
+
+
+
+# ===== CONDITIONALS =====
+
+# In Codecademy, they called this lesson "Controlling the flow"
+
+# Flow here means flow of the code
+# By default, our code runs from top to bottom
+# Controlling the flow will let us "Jump Around"
+
+
+### Theory
+
+if condition
+    # This part will run if the condition is true
+elsif another_condition
+    # This code will run if another_condition is true
+else # NO CONDITION HERE
+    # This code will run in any other case
+end
+
+# Logical Operators
+
+2 == 3 # Checks Equality, in that case it will return false
+2 != 3 # Checks inequality, in that case it will return true
+
+2 > 3 # greater than, in that case returns false
+2 < 3 # smaller than, in that case returns true
+
+&& # Logical and
+|| # logical or
+
+!true # 'not' operator, in that case, returns false
+
+
+# We can store those conditions:
+result = 2 > 3
+puts result # => false
+
+# Ruby is truthy
+# In Ruby, everything is true except... false and nil
+
+if 'sylvain'
+    puts 'This was considered true'
+end
+
+# Will print 'This was considered true'
+
+
+# ===== LIVECODE 1 =====
+
+# Code the opening hours of a restaurant
+
+puts 'Welcome to Jiro! 🍣🍣🍣'
+puts 'What time is it now?'
+print '> '
+time = gets.chomp.to_i
+
+dinner_time = time >= 19 && time <= 23
+lunch_time = time >= 12 && time <= 15
+
+if dinner_time
+    puts 'The restaurant is open for omakase dinner!'
+elsif lunch_time
+    puts 'The restaurant is open for lunch!'
+elsif time == 11
+    puts 'The restaurant opens soon!'
+else
+    puts 'Sorry, the restaurant is closed... 😭'
+end
+
+###  Boolean Logic
+
+# with &&, returns true if all conditions are true
+true && true && false # => false
+# with ||, returns true if at least one condition is true
+true || false || false # => true
+
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,97 @@
 ### Looping with Ruby
+
+
+# ===== LOOPING =====
+
+# Looping is THE reason why computers are powerful
+
+# Let's introduce a new Data Type: Ranges
+1..10 # is a range from 1 to 10
+1...10 # is a range from 1 to 9
+puts (1..10).class # => Range
+p ('a'..'z').to_a # => will return the 'Alphabet Array'
+
+
+### Three not very used ways to loop in Ruby
+
+### for loop
+
+# for loops are not the way we do things in Ruby
+
+for i in 1..10
+    puts i
+end
+
+
+### loop do
+
+counter = 0
+loop do
+    puts 'I love Ruby!'
+    counter += 1
+    break if counter == 10
+end
+
+
+### .times 
+
+10.times do 
+    # puts 'I love Ruby 😘'
+end
+
+
+# ===== LIVECODE 2 =====
+
+# Let's code the Price is Right
+# A participant can guess a price in as many tries as they want
+
+# Pseudo-Code:
+# 0) Welcome the user to our game! OK
+# 1) Ask user to guess a price OK
+# 2) Set a price to guess OK
+# 3) gets user input and store it in a variable (cast the user input as an Integer!) OK
+# 4) Loop *until* user guesses the correct answer
+
+puts 'Welcome to the price is right! 🤑'
+
+# Set a price to guess
+price_to_guess = rand(1..10)
+
+# Initializing user_guess
+user_guess = 0
+
+until user_guess == price_to_guess # loop until condition is met, i.e. user guesses the correct price
+    puts 'Guess a price from 1 to 10'
+    print '> '
+    user_guess = gets.chomp.to_i 
+end
+
+# End of game message
+puts 'Congratulations, you guessed correctly!'
+
+# Homework
+# 1) Give a hint to the user whether their guess is high or low
+# 2) Count the number of tries it took to guess
+# 3) Bonus: Set a maximum number of tries
+
+
+
+### Another way to loop: Iterators
+
+# Iterators are a way to go over a collection of elements
+
+team_members = ['Sasha', 'Douglas', 'Yann', 'Sylvain']
+
+# Iterate over an array with a for loop:
+
+for member in team_members
+    puts "#{member} works for Le Wagon Tokyo"
+end
+
+
+# The Ruby way to iterate over an array, using an iterator, .each :
+
+team_members.each do |member|
+    puts "#{member} works for Le Wagon Tokyo"
+end
+
