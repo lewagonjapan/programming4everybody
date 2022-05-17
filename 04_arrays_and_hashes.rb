5c9fecd44de1894fa3acda41c00753a454ed269e
Sylvain Pierre <sylvain.pierre@officience.com>
2022-05-17 21:52:36 +0900

    Added Slides and Livecode for session 3

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,70 @@
 
-### Arrays
+### ===== ARRAYS =====
+
+### Intro about Arrays
+
+# Arrays are delimited by square brackets
+[].class  # => Array
+[1, 2, 3] #  => array of 3 Strings
+
+# Ruby doesn't care about the type of objects you store in an array
+[1, 'le wagon', 1.3, true]
+# Most of the time, all the elements are of the same class / data type
+
+### In an Array, we start counting from 0
+#  The position of an element is called its index
+['sasha', 'douglas', 'yann', 'sylvain']
+#   0         1         2        3
+
+
+### CRUD for Arrays
+
+# CRUD stands for Create Read Update Delete
+# CRUD are the basic operations you can do with a database
+
+le_wagon_team = ['sasha', 'douglas', 'yann', 'sylvain']
+
+### Read
+# Accessing an element in an array
+#Theory: array[index]
+
+puts le_wagon_team[1]
+p le_wagon_team[0..2]
+puts le_wagon_team[-1]
+
+puts le_wagon_team.first
+puts le_wagon_team.last
+
+
+### Create
+# Adding a new element in an array
+# Theory: array.push(item_to_add) OR array << item_to_add
+le_wagon_team.push('james')
+p le_wagon_team
+
+le_wagon_team << 'aime'
+p le_wagon_team
+
+
+### Update
+# Changing one element in an array
+# Theory: array[index_to_update] = new_item
+le_wagon_team[1] = 'tim'
+p le_wagon_team
+
+
+### Delete
+# Removing items from an array
+# Theory:
+# array.delete(item_to_delete)
+# array.delete_at(index_to_delete)
+
+le_wagon_team.delete('tim')
+p le_wagon_team
+
+le_wagon_team.delete_at(-1)
+p le_wagon_team
+
+# Two dimensional arrays
+array = [[1, 2] ,[3, 4] ,[5, 6]] # Array of 3 arrays
+puts array.last  # => [5, 6]
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,166 @@
 
 ### Methods
+
+
+### ===== ITERATORS AND BLOCKS =====
+
+### Iterators
+
+# An iterator is a way to go over each element of an array and 'do something'
+
+radiohead = ['phil', 'thom', 'ed', 'jonny', 'colin']
+
+#### .each
+# .each allows us to go over each element of an array
+
+radiohead.each do |member|
+    puts "#{member.capitalize} plays for Radiohead"
+end
+
+# every other iterator can be replicated using .each
+
+
+#### .each_with_index
+radiohead.each_with_index do |member, index|
+    puts "#{index + 1}) #{member.capitalize} plays for Radiohead"
+end
+
+# Replicating .each_with_index with .each
+index = 0
+radiohead.each do |member|
+    puts "#{index + 1}) #{member.capitalize} plays for Radiohead"
+    index += 1
+end
+
+#### .map
+# .map lets us go over an array, apply a transformation to each element, and returns the transformed array
+
+numbers = [1, 2, 3, 4]
+
+transformed_numbers = numbers.map do |number|
+    # transformation
+    number * 3
+end
+
+# [ 1  ,  2   ,  3    ,  4]
+# ===== Transformation (*2) ====
+# [ 2  ,  4   ,  6    ,  8]
+
+radiohead = ['tim', 'thom', 'ed', 'jonny', 'colin']
+
+capitalized_radiohead = radiohead.map do |member|
+    member.capitalize # Transformation    
+end
+
+# .map does not modify the original array
+
+
+# .map
+# is called on an array
+# specify a *transformation* in the block
+# .map *returns* an array of the same size as the original array
+
+
+#### .select
+
+# select lets us select some elements from an array, based on a specific condition passed in the block
+
+p_members = radiohead.select do |member|
+    member.chars.first == 't' # condition
+end
+
+# .select
+# is called on an array
+# we specify a *condition* in the block
+# select returns an array with all the elements for which the condition is true
+
+
+# For each iterator, be careful:
+# Waht are they called on
+# What to write in the block
+# What do they return
+
+
+#### Blocks
+
+# a block starts with 'do' and end with 'end'
+# OR is between { }
+
+# multi-line syntax
+capitalized_radiohead = radiohead.map do |member|
+    member.capitalize # Transformation    
+end
+
+# one-line block syntax
+cap_radiohead = radiohead.map { |member| member.capitalize }
+
+p capitalized_radiohead
+p cap_radiohead
+
+
+### ===== METHODS =====
+
+def beautified_name(first_name, last_name)
+    cap_first = first_name.capitalize
+    cap_last = last_name.capitalize
+    full_name = cap_first + ' ' + cap_last
+
+    return full_name
+end
+
+puts beautified_name('sylvain', 'pierre')
+
+# puts 'what is your first name?'
+# first = gets.chomp
+
+# puts 'what is your last name?'
+# last = gets.chomp
+
+# puts beautified_name(first, last)
+
+
+# Developers are LAZY
+# They hate repeating themselves
+# Methods let us store some code, to be able to re-use it later
+
+# Understand the difference between method *definition* and *calling* the method
+
+
+# Conventions about Methods:
+# - method name is always written in lower_snake_case
+# - ? methods always return true or false (.odd?, .even?, .include?)
+# - ! methods are called destructive or dangerous: they modify the object they are called on
+
+
+### ===== LIVECODE =====
+
+# DISCLAIMER: This is an ADVANCED livecode
+
+# Scrape prices from the Rakuten website
+# 1) Be able to open a webpage
+# 2) understand the structure of an HTML file
+# 3) find the elements in the HTML that include prices
+# 4) extract those elements
+
+# Ruby gem (=library) to be able to open a web page
+require 'open-uri'
+
+# Ruby gem to be able to parse (break down) the HTML
+require 'nokogiri'
+
+puts 'What prices would you like to check on Rakuten?'
+print '> '
+keyword = gets.chomp
+
+# opening Rakuten page and storing the HTML code
+html = open("https://search.rakuten.co.jp/search/mall/#{keyword}/").read
+
+# Parsing the huge string we got from Rakuten
+parsed_html = Nokogiri::HTML(html)
+
+# Search in the HTML the elements that include prices
+puts "These are today's prices for #{keyword}:"
+prices = parsed_html.search('.price .important').first(10).map do |element|
+    element.text.strip
+end
+
