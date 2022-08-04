c2809b701446c79d2ba1f696a5a1723e0ba27ff4
Sylvain Pierre <sylvain.pierre@officience.com>
2022-08-04 21:53:56 +0900

    Added Slides and Livecode for Session 2

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,133 @@
 ### Controling Ruby Flow
+
+
+# ==== RECAP ====
+
+#  Last time we saw: Data Types & Variables
+
+### Data Types
+
+# Strings
+'Sylvain'
+"Sylvain"
+
+4 # Integer
+-4 # Integer
+4.3 # Float
+-1.5 # Float
+
+true # TrueClass
+false # FalseClass
+
+[1, 2, 3] # Array
+
+### Variables
+age = 23
+
+# The Integer 23 is *assigned* to the variable called age
+# = is called the assignment operator
+
+### Miscellaneous
+
+# getting input from the user
+
+puts 'How old are you?'
+print '> '
+user_age = gets.chomp.to_i # Gets user input AS A STRING
+
+# Interpolation
+
+# Interpolation is a way to inject Ruby code into a String
+# It only works with ""
+
+puts "Next year you will be #{user_age + 1}!"
+
+
+
+# === CONDITIONALS ===
+
+# Controlling the flow
+
+# So far, our code has been running from top to bottom
+# The flow of our code is top to bottom
+
+# controlling the flow lets us change that
+
+
+### Theory
+
+if condition
+    # run this code if the condition is true
+elsif another_condition
+    # run this code if another_condition is true
+else # NO CONDITION HERE
+    # any other case
+end
+
+
+## Logical Operators
+
+# Checking equality and difference
+4 == '4' # => false
+4 != '4' # => true
+
+# Checking greather than / less than
+4 >= 3 # =>  true
+4 <= 3 # =>  false
+
+# Boolean logic operators
+# && # Logical and
+# || # Logical or
+
+# 'Not' operator
+!true # => false
+
+# ⚠️ this is different from .capitalize!
+
+# We can 'store' the result of those operations
+result = 4 > 3
+# puts result
+
+
+## Ruby is truthy
+# Everything in Ruby is true, except for... nil and false
+
+if 'khan'
+    puts 'This was considered true'
+end
+
+
+# === LIVECODE ===
+
+# Let's code the opening hours of a restaurant
+
+puts 'Welcome to Jiro Sushi!! 🍣🍣🍣'
+puts  ''
+puts 'What time is it now?'
+print '> '
+time = gets.chomp.to_i
+puts ''
+
+dinner_time = (time >= 19) && (time <= 23)
+lunch_time = (time >= 12) && (time <= 14)
+
+if time == 11
+    puts 'The restaurant opens soon 🤤'
+elsif dinner_time
+    puts 'Jiro is open for dinner!'
+elsif lunch_time
+    puts 'The restaurant is open for lunch!'
+else
+    puts 'Sorry, the restaurant is closed... 😭'
+end
+
+# With several conditions, always put the strictest conditions first
+
+
+# Boolean Logic
+
+# With &&, will return true if all elements are true
+true && true && false # => false
+
+# With ||, returns true when at least one element is true
+true || false || false # => true
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,94 @@
 ### Looping with Ruby
+
+# ==== LOOPING ====
+
+# Looping is what makes computers powerful
+
+
+# New Data Type: Range
+1..10 # all the integers from 1 to 10
+1...10 # all the integers from 1 to 9
+
+(1..10).to_a
+('a'..'z').to_a
+
+
+### Three ways to loop that are almost never used in Ruby
+
+# 1) for loop
+
+for i in 1..10
+    puts i
+end
+
+# 2) loop do
+
+counter = 0
+loop do
+    puts 'I love Ruby 😍'
+    counter += 1
+    break if counter == 10
+end
+
+# 3) .times do
+
+10.times do
+    puts 'I love Ruby 😘'
+end
+
+
+# The 'Correct' way to loop:
+# while / until
+
+
+# ==== LIVECODE: PRICE IS RIGHT ====
+
+# Our user can guess a price until they find the correct price
+
+# Pseudo-Code
+# 0 - Greet the user! OK
+# 1 - Set a price to guess OK
+# 2 - Ask the user for their guess
+# 3 - Keep asking *until* the user guesses the correct price
+# 4 - Final message when the user guesses correctly
+
+puts 'Welcome the The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+# Setting the price to guess
+price_to_guess = rand(1..10)
+
+# Initialize user_guess
+user_guess = 0
+
+# loop starts
+message = 'Guess a price between 1 and 10'
+until user_guess == price_to_guess
+    puts message
+    message = 'Try again!'
+    print '> '
+    user_guess = gets.chomp.to_i # Casting user input as an Integer
+end
+
+puts ''
+puts 'Congrats! You guessed the right price'
+
+
+### Iterators
+
+le_wagon_team = ['Sasha', 'Douglas', 'Yann', 'Sylvain']
+
+# Iterating over an array with a for loop:
+
+for member in le_wagon_team
+    puts "#{member} works at Le Wagon"
+end
+
+
+# With an iterator: the most basic iterator is .each
+
+le_wagon_team.each do |member|
+    puts "#{member} works at Le Wagon"
+end
+
