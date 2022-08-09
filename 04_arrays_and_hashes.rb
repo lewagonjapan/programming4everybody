0f98cc4bc58502f6d4fa0d90dcb5d4570df7c5a8
Douglas Berkley <douglasmberkley@gmail.com>
2022-08-09 20:17:19 +0900

    livecode and slides for Tues Aug 9th

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,44 @@
-
 ### Arrays
+
+# Arrays are created with square brackets
+empty_array = []
+array = [1, 2, 3]
+
+# Ruby doesn't care about the data types in the array
+array = [1, 'hello', [], 1, 2]
+
+# we access the information inside of an array using the index
+students = ['michael', 'shiori', 'paloma', 'marco', 'sarah']
+# index         0         1         2         3         4
+
+# CRUD - basic operations that you can do to a database
+
+# Create
+# Theory: array.push('leah')
+# Theory: array << 'leah' (places the new value at the end)
+# students.push('leah')
+students << 'leah'
+
+# Read
+# Theory: array[index]
+students[0]
+# read the last one in the array
+students[-1]
+# return nil for an index that doesn't exist
+students[10]
+# read the first three name in our array
+students[0...2]
+# read the index of a student
+students.index('marco')
+
+# Update
+# Theory: array[index] = new_value
+students[0] = 'emily'
+students[-1] = 'alex'
+
+# Delete
+# Theory: array.delete(value)
+# Theory: array.delete_at(index)
+students.delete('alex')
+students.delete_at(3)
+p students
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,83 @@
 
-### Methods
+### Methods && Blocks
+
+# Block
+# A block is our do...end
+
+# multi line syntax
+# array.each do |element|
+#   # do something to that element
+# end
+
+# single line syntax
+# array.each { |element| 'do something to that element' }
+
+
+# An iterator is a way to go over EACH element on an array and 'do something'
+students = ['michael', 'shiori', 'paloma', 'marco', 'sarah', 'emily']
+
+# .each goes over the indivual element in the array (just gives the original array back)
+students.each do |student|
+  # "#{number}.) #{student.capitalize} is taking programming for everyone!"
+end
+
+# inside of the |pipes|, we have our parameters
+students.each_with_index do |student, index|
+  "#{index + 1}.) #{student.capitalize} is taking programming for everyone!"
+end
+
+# build a new array using .each....
+capitalized = []
+students.each do |student|
+  capitalized << student.capitalize
+end
+
+# to BUILD a NEW array we use .
+# .map -> iterate over each thing, and places the LAST LINE inside of a NEW array
+capitalized = students.map do |student|
+  "sarah"
+  student.capitalize  # will take the last line inside of the block
+end
+# p capitalized
+
+students.select do |student|
+  student[0] == 'm'
+end
+
+m_names = students.count do |student|
+  student[0] == 'm'
+end
+
+# .each -> returns the original array
+# .map -> builds a new array
+# .select -> filter the original array into a new array
+# .count -> integer (for which is true)
+
+
+# METHODS
+# a block of reusable code which returns a dynamic value
+# DRY - Don't Repeat Yourself
+
+# define a method -> def method_name end
+# method signature -> 1. name? 2. take any arguments? 3. what does it return?
+
+# you dont' need the word return. In Ruby, the last statement will be returned
+
+# first_name and last_name are PARAMETERS -> placeholders
+def beautify_name(first_name, last_name)
+  cap_first = first_name.capitalize
+  cap_last = last_name.capitalize
+  # return cap_first + " " cap_last
+  "#{cap_first} #{cap_last}" # will return this line bc it's last
+end
+
+puts beautify_name('michael', 'smith')
+puts beautify_name('sarah', 'smith')
+puts beautify_name('emily', 'smith')
+
+# Naming conventions
+# variables and methods -> lower_snake_case
+# Class -> UpperCamelCase
+
+# 12.even? => return a boolean
+# 12.even? => return a boolean
