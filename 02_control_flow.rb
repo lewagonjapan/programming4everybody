902c8193a4f6edf0ea17edcbcc9a7364510dbf8d
soufiane ezzine <ezzine.soufiane@gmail.com>
2023-07-27 21:29:51 +0900

    Session2 Livecode

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,80 @@
 ### Controling Ruby Flow
+
+# conditionals
+# Concepts
+# syntax
+if nil
+  # block of code that is going to run if the condition is true.
+else
+  # block of code that is going to run if the condition is false
+end
+
+unless condition
+  # block of code that is going to run if the condition is false
+else
+  # block of code that is going to run if the condition is true.
+end
+
+# condition
+# is an expression that return true of false
+
+# expression
+# 2 + 2 # arithmetic expression
+# !(2 - 1) # !1
+# 2 / 4
+# x = 20
+# y = x + 5 # using the variable x is an expression
+
+# 2 == 3 # boolean expression
+# 2 > 4 # boolean expression
+# 3 < 4
+# 2 >= 2
+# 2 <= 2
+
+# Logical NOT (!)
+# !true == false
+# !false == true
+
+# nil value
+name = nil
+
+# Everything in ruby is true (any data type)
+# exept for false and nil
+
+# Logical AND / OR
+# && # condition && condition
+# || # condition || condition
+
+# For the the condition to return true
+# If I am using and AND logical operator (All the sides of the operator have to be true)
+# if I am using the OR local operator (At least one of the sides has true)
+
+# livecode
+# I have a restaurant
+# Program to check if the restaurant is open at a given hour
+
+# pseudocode
+# 1. welcome the user to the app
+# 2  ask the user what time they are inquiring about
+# 3. get the user answer / store it in a variable ( so we can reuse it)
+# 4. compare the user answer to the opening hours of the restaurant
+# 5. print the correct information to the user
+
+puts "Welcome to Jiro sushi 🍣"
+puts " "
+puts "What time are you inquiring about?"
+print "> "
+
+time = gets.chomp.to_i # 20
+noon = time >= 12 && time < 15
+dinner = time >= 19 && time < 23
+
+if time == 11 # the most specific
+  puts "The restaurant is opening soon."
+elsif dinner
+  puts "The restaurant is open for dinner"
+elsif noon
+  puts "The restaurant is open for lunch."
+else # no condition goes here
+  puts "The restaurant is closed."
+end
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,94 @@
 ### Looping with Ruby
+
+# Allows a set on instruction or code to be executed *repeatedly*
+# based on a certain condition
+
+# iteration is 1 cycle of the loop
+
+
+# New data type
+# Range
+
+(1..10).to_a # a range from 1 to 10
+p (1...10).to_a # a range from from 1 to 9 (10 excluded)
+p ('a'..'z').to_a # A range from a to z
+
+
+# For loop
+
+for number in (1..10)
+  puts "My number is #{number}"
+end
+
+# loop do
+counter = 0
+
+loop do
+  puts 'I love coding!'
+  counter = counter + 1
+  if counter == 10
+    break # keyword to break out of a loop
+  end
+end
+
+# .times loop
+
+10.times do
+  puts "I love Coding"
+end
+
+# live code
+# Pseudo code
+
+# 1. Greet the user
+# 2. set a price for product
+# 3. ask the user to guess a price
+# 4. keep asking the user to guess while / until they guess right
+# 5. final message when they guess correctly
+
+# 1. Greet the user
+puts 'Welcome The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+# 2. set a price for product
+price_to_guess = rand(1..10)
+
+user_guess = 0
+message = 'Guess a price between 1 and 10'
+
+# 3. ask the user to guess a price
+# keep asking the user to guess while / until they guess right
+until price_to_guess == user_guess
+  puts message
+  message = 'try again!'
+  user_guess = gets.chomp.to_i
+end
+
+# 5. final message when they guess correctly
+puts ""
+puts "Congratz you guessed it right."
+
+
+
+# array = ["soufiane", "Ike", "Eli"]
+
+for number in array
+  puts "My number is #{number}"
+end
+
+# iterators
+array.each do |n|
+  puts "My name is #{n}."
+end
+
+array.each { |number| puts "My number is #{number}" }
+
+array.each_with_index do |number, index|
+  puts "#{index + 1}. My number is #{number}"
+end
+
+# [1, 2, 3]
+# ["str1", "str2"]
+# [1, "str2", true, 0]
+# array = [[1, 2, 3], ["str1", "str2"], [true], 4]
