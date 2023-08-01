201ddf1fde9184a7ac53f4608ccec0164e8db0c7
soufiane ezzine <ezzine.soufiane@gmail.com>
2023-08-01 23:40:22 +0900

    session 3 livecode

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,119 @@
-
 ### Arrays
+
+students = ['alajandra', 'rikka', 'mae', 'heinrich', 'nishan', 'mei']
+# indexes        0           1       2        3          4        5
+# reverse       -6          -5      -4       -3         -2       -1
+
+# CRUD
+# create
+[].class
+
+# two main ways to add into an array
+# array << element
+students << 'joe' # shovel operator
+# array.push('element')
+students.push(23) # push method
+
+
+# Read
+# array[index]
+students[2]
+# last element
+students[-1]
+# portion of the the array
+students[0...2]
+
+# get the index of an element
+# array.index(element)
+students.index('mei')
+p students.index('something') # => nil
+
+# p students[10]
+
+# Updated
+# array[index] = new_value
+students[0] = 'hide'
+students[-1] = 'hide'
+
+# Delete
+# array.delete(delete)
+students.delete('rikka')
+# array.delete_at(index)
+students.delete_at(4)
+# p students
+
+# Iterators
+# Iterators are tools I have at my disposale to loop over arrays and do something
+# Go over each element in the array and do something with each element depending on the task
+# also depending on which iterator I chose
+
+# syntax
+
+# Multiline syntax
+# array.iterator do |element|
+#   # do something
+#   # do something else
+# end
+
+# single line syntax
+# array.iterator { |element|  #do somethind }
+
+
+students = ['alajandra', 'rikka', 'mae', 'heinrich', 'nishan', 'mei']
+
+### .each
+
+students.each do |student|
+  puts "#{student.upcase}"
+end
+
+# I want to build a new array using .each
+capitalized = []
+
+students.each do |student|
+  # capitalized << student.capitalize
+  capitalized.push(student.capitalize)
+end
+
+p capitalized
+
+### .map
+p students
+
+upcased = students.map do |student|
+  student.upcase
+end
+
+p upcased
+p students
+
+
+### destructirve map
+p students # => print to check the state of the array before the destructive map
+
+upcased = students.map! do |student|
+  student.upcase
+end
+
+p upcased # => the new array that the .map constructed
+p students #=> the state of the students array after destructive map!
+
+# capitalized
+# p students
+
+### .select
+# filter an array and resturns a new array with only the elements
+# that return true for the condition.
+m_students = students.select do |student|
+  student[0] == 'm'
+end
+
+### .count
+
+# if given a block (do ... end) .count will count the number of element
+# that return true for the condition in the block
+number = students.count do |student|
+  student[0] != 'm'
+end
+
+p number
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,39 @@
+### Methods & blocks
 
-### Methods
+# methods
+
+# Variables => ia a way to store data in order to reuse it later
+# methods / functions allow us to store a chunck of code / logic so we can reuse it later
+
+# why ?
+# stay DRY => Dont Repeat Yourself
+# to breakdown a large problem into smaller pieces
+
+
+# I define the method/ function
+def beautiful_name(first_name, last_name)
+  cap_fname = first_name.capitalize
+  cap_lname = last_name.capitalize
+
+  full_name = "#{cap_fname} #{cap_lname}"
+
+  return full_name
+end
+
+def add_number(num1, num2)
+  return num1 + num2
+end
+
+# I call the method / use the method
+p beautiful_name('soufiane', 'ezzine')
+p beautiful_name('mahmoud', 'takubo')
+
+# The distinction between defining a method and calling / using the method
+# I define a method with parameters / call the method with arguments
+# A method will alwaus return the line of code
+# the return keyword is optional in ruby
+
+## Naming convention
+# - method names are always lower_snake_case
+# - methods ending with ? always return a boolean
+# - methods ending with ! (dangerous methods / destructive methods) they modify the original data
