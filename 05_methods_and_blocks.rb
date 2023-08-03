6e83876161627b969419abff663d04aa7f144e0f
soufiane ezzine <ezzine.soufiane@gmail.com>
2023-08-03 22:26:45 +0900

    added slides

--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -3,12 +3,11 @@
 # methods
 
 # Variables => ia a way to store data in order to reuse it later
-# methods / functions allow us to store a chunck of code / logic so we can reuse it later
+# methods / functions allow us to store a chunck of code so we can reuse it later
 
 # why ?
 # stay DRY => Dont Repeat Yourself
 # to breakdown a large problem into smaller pieces
-
 
 # I define the method/ function
 def beautiful_name(first_name, last_name)
@@ -20,13 +19,15 @@ def beautiful_name(first_name, last_name)
   return full_name
 end
 
-def add_number(num1, num2)
-  return num1 + num2
-end
+# def add_number(num1, num2)
+#   return num1 + num2
+# end
 
 # I call the method / use the method
-p beautiful_name('soufiane', 'ezzine')
-p beautiful_name('mahmoud', 'takubo')
+# p beautiful_name('soufiane', 'ezzine')
+
+# p beautiful_name('mahmoud', 'takubo')
+
 
 # The distinction between defining a method and calling / using the method
 # I define a method with parameters / call the method with arguments
