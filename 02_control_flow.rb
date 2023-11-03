cd5b7fc39239fafc2c30b3b6c195beb69f69eeec
soufiane ezzine <ezzine.soufiane@gmail.com>
2023-11-03 17:16:04 +0900

    Add code and slides for session 2

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,72 @@
 ### Controling Ruby Flow
+
+# theory
+
+# if condition
+#   puts "this code executes if the condition is true"
+# else
+#   puts "this code executes if the condition is false"
+# end
+
+# expression
+2 == 4 # => Equality
+2 != 4 # => inequality
+2 > 4 # => greater
+2 >= 4 #=> greater or equal
+2 < 4 #=> less
+2 <= 4 # => less or equal
+
+
+#### In ruby all data types are truthy / true excpet for false / nil
+# if nil
+#   puts "true"
+# else
+#   puts "false"
+# end
+
+#livecode
+# Pseudo code
+
+#Logical AND / OR
+# && Logical AND -> Return true only if all the sides of the && are true
+# || Logical OR -> return true if at least one of the sides is true
+
+#program to check if a restaurant is open
+
+# 1- Welcome the user to the program (ok)
+# 2- Ask the user what time he is asking for? (ok)
+# Get the user answer/input store in a variable so we can reuse it (ok)
+# Compare the user asnwer to the opening hours (ok)
+# Print the correct informatio
+
+puts "Welcome to Jiro Sushi 🍣"
+puts ''
+puts 'What time are you inquiring about?'
+print '> '
+
+time = gets.chomp.to_i
+noon = time >= 12 && time < 15
+evening = time >= 19 && time < 23
+
+if time == 11
+  puts "The restaurant is opening soon"
+elsif evening
+  puts "The restaurant is open for dinner"
+elsif noon
+  puts "The restaurant is open of lunch"
+else
+  puts "The restaurant is closed"
+end
+
+# logical not (!)
+!true == false
+!false == true
+
+# if !condition
+# unless condition
+#   puts "if the condition is false"
+# else
+#   puts "if condition is true"
+# end
+
+
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,73 @@
 ### Looping with Ruby
+
+# Range
+(1..10).to_a
+(1...10).to_a
+('a'..'z').to_a
+
+# for
+for name in names
+  # puts "My friend is #{name}!"
+end
+
+# loop do
+counter = 0 # intialize a counter
+
+loop do
+  # puts "I love coding!"
+  counter += 1
+  # counter = counter + 10
+  if counter == 10
+    break # breaks out of the loop
+  end
+end
+
+# .times
+10.times do
+  puts "I love coding"
+end
+
+10.times { puts "I love coding" }
+
+# Livecode ( Pseudo )
+# 1- Greet the user
+# 2- set the price of the product to guess
+# 3- ask the user for a price
+# 4- store it in a variable
+# 5- compare the user input the products price
+# 6- Keep asking until the guess is correct
+# 7- final message to thte user
+
+puts 'Welcome The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+price_to_guess = rand(1..5)
+
+puts "Guess  price between $1 and $5"
+print '> '
+
+user_guess = gets.chomp.to_i
+
+# until price_to_guess == user_guess
+while price_to_guess != user_guess
+  puts "Try again!"
+  print '> '
+  user_guess = gets.chomp.to_i
+end
+
+puts "You guessed it the correct price was $#{price_to_guess}"
+#
+#
+names = ["Soufiane", "Vrushali", "David"]
+
+for name in names
+  puts "My name is #{name}"
+end
+
+names.each do |name|
+  puts "Hello there,"
+  puts "My name is #{name}"
+end
+
+names.each { |name| puts "My name is #{name}" }
