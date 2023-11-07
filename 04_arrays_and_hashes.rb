518802fd5814678c1accf54508ff21797ea9f570
soufiane ezzine <ezzine.soufiane@gmail.com>
2023-11-07 21:00:40 +0900

    Add code and slides for session 3

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,103 @@
-
 ### Arrays
+# [].class
+#
+students = ['alejandra', 'rikka', 'mae', 'heinrich', 'nishan', 'mei']
+# index          0            1      2        3          4        5
+# reverse      -6           -5      -4      -3         -2       -1
+
+# CRUD
+
+# Create
+# array << element
+students << "glen"
+
+# array.push(element)
+students.push("jonathan")
+
+# Read
+# array[0]
+p students[0]
+p students[-1]
+
+# array[range]
+students[0..2]
+
+# array.index(element)
+p students.index('rikka')
+
+# try to read an element at an index that doesnt exist
+p students[10] #=> nil
+p students.index("something") #=> nil
+
+# Update
+# array[index] = new_value
+students[0] = "yuzu"
+students[-1] = "glen"
+
+# Delete
+# array.delete(element)
+students.delete("mae")
+# array.delete_at(index)
+students.delete_at(-1)
+
+p students
+
+students = ['alejandra', 'rikka', 'mae', 'heinrich', 'nishan', 'mei']
+
+# syntax
+# array.iterator do |element|
+#   # Do something with that element
+# end
+
+each_student = students.each do |student|
+  name = student
+  puts "#{name.capitalize} is a good student!"
+end
+p each_student
+
+# short hand to write a one line blocks
+students.each { |student| puts "#{student.capitalize} is a good student!"}
+
+# .each_with_index
+students.each_with_index do |student, index|
+  # puts "#{index + 1} - #{student}"
+end
+
+# I want to create a new array with capitalized names
+capitalized = []
+students.each do |student|
+  capitalized << student.capitalize
+end
+
+# p capitalized
+
+# .map
+capitalized = students.map do |student|
+  # student.capitalize
+end
+
+# p capitalized
+
+# .select / .filter filters the array leavng only element that meet the criteria
+m_students = students.select do |student|
+  # student[0] == 'm' #=> boolean true / false
+end
+
+# p m_students
+
+# .count
+m_students = students.count do |student|
+  # student[0] == 'm'
+end
+
+# p m_students
+
+students.size
+students.length
+
+
+# some string operations
+str = "Glen"
+str[-1] = "F"
+p str
+
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,73 @@
-
 ### Methods
+
+# variable is a way to store values to be reused
+# Methods / functions allow us to store a chunk of code to be reused
+
+# why ?
+# stay DRY => Don't Repeat Yourself
+# Break down a large problem (coding) in smaller chunks
+
+# def method_name(parameter1, parameter2)
+#   return
+# end
+
+first_name = "soufiane"
+last_name = "ezzine"
+
+# cap_fname = first_name.capitalize
+# cap_lname = last_name.capitalize
+#
+# full_name = "#{cap_fname} #{cap_lname}"
+#
+# p full_name
+
+def beautiful_name(fname, lname) # parameters
+  cap_fname = fname.capitalize
+  cap_lname = lname.capitalize
+
+  full_name = "#{cap_fname} #{cap_lname}"
+
+  full_name
+end
+
+p beautiful_name("david", "clement") # arguemnts
+p beautiful_name("soufiane", "ezzine")
+p beautiful_name("jonathan", "krutz")
+
+def squareNumber(number)
+  return number * number
+end
+
+p squareNumber(2)
+p squareNumber(10)
+p squareNumber(452)
+
+# Distinction between defining a method and calling a method
+# I define a method with parameters / Call a method with arguments
+# A method will always return the last line
+# the return keyword is option in ruby
+
+# Naming convention
+# - methods are alway lower_sname_case
+# - methods that end with "?"
+def is_even?(number)
+  return number % 2 == 0
+end
+
+p 2.odd?
+p 45.even?
+
+p is_even?(2)
+p is_even?(45)
+p is_even?(60)
+
+# - methods that end with "!" (destructive methods)
+# They modify the original data that is passed to them
+
+def add_bang!(str)
+  str.replace(str + '!')
+end
+
+my_string = "Hello"
+p add_bang!(my_string)
+p my_string
