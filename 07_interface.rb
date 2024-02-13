15855a1deabc20c2d370a613271dbdffb86181bb
Douglas Berkley <douglasmberkley@gmail.com>
2024-02-13 22:37:19 +0900

    final livecode

--- 07_interface.rb
+++ 07_interface.rb
@@ -1,1 +1,49 @@
-# *Blocks recap*
+# we created the class Warrior in the 07_oop file
+require_relative '07_oop'
+
+# Communicate with the user
+# puts / gets.chomp
+
+puts "Who is Player 1?"
+name_one = gets.chomp
+puts "Who is Player 2?"
+name_two = gets.chomp
+
+player_one = Warrior.new(name_one, Warrior.weapons.keys.sample)
+player_two = Warrior.new(name_two, Warrior.weapons.keys.sample)
+
+puts "*********************"
+puts "Welcome the Le Wagon Thunderdome ⚔️"
+puts "*********************"
+puts "Battling today, we have #{player_one.name} vs. #{player_two.name}"
+sleep(2)
+puts "Fight!!!"
+
+until player_one.dead? || player_two.dead?
+  # Player one attacking...
+  puts "#{player_one.name} attacks #{player_two.name} with a #{player_one.weapon}!"
+  # player_one attack player_two
+  player_one.attack(player_two)
+  sleep(2)
+  puts "#{player_two.name}'s health: #{player_two.health}"
+  sleep(2)
+  unless player_two.dead?
+    # player_two attack player_one
+    puts "#{player_two.name} attacks #{player_one.name} with a #{player_two.weapon}!"
+    player_two.attack(player_one)
+    sleep(2)
+    puts "#{player_one.name}'s health: #{player_one.health}"
+    sleep(2)
+    player_one.weapon = Warrior.weapons.keys.sample
+    player_two.weapon = Warrior.weapons.keys.sample
+  end
+end
+
+# display results
+if player_one.health > player_two.health
+  puts "#{player_one.name} is the champion!"
+elsif player_two.health > player_one.health
+  puts "#{player_two.name} is the champion!"
+else
+  puts "Everyone died...."
+end
--- 07_oop.rb
+++ 07_oop.rb
@@ -1,2 +1,103 @@
-# Syntax Sugar
-# syntactic sugar is syntax within a programming language that is designed to make things easier to read or to express. It makes the language "sweeter" for human use
+# OOP = OBJECT ORIENTED PROGRAMMING
+
+# Everything in Ruby is an object!
+# OOP = Data + Behavior
+
+# data -> the information we want to store
+# behavior -> actions we want to run on that data (aka methods)
+
+"Sergio" # data
+"sergio".upcase # behavoir
+"sergio".downcase # behavoir
+"sergio".capitalize # behavoir
+"sergio".reverse # behavoir
+"sergio".split('') # behavoir
+
+# Prebuilt Objects
+String # (.upcase, .downcase)
+Array # (.join, .first, .push)
+Integer # (.even?, .odd?, .positive?)
+
+# Creating own classes that stores data and runs behavior
+String   # class    -> cake mould makes the cake
+'sergio' # instance -> cake
+
+# Naming conventions
+# Class -> UpperCamelCase
+# methods/variables -> lower_snake_case
+
+# defining a method
+def method_name
+  # something
+end
+
+# defining a class
+class ClassName
+end
+
+# to create an instance of a warrior:
+# -> Warrior.new
+# .new -> creates the instance AND triggers the method .initialize
+
+# data + behavior
+class Warrior
+
+  # set the initial data for my instance
+  def initialize(name, weapon)
+    # we store the initial data in @instance variables
+    @name = name
+    @weapon = weapon
+    @health = 100
+  end
+
+  # instance method!
+  # sergio.health
+  # joe.health
+  def health
+    # when we are inside of instance methods, we have access to ALL instance variables
+    return @health
+  end
+
+  # attr_reader -> a method that reads a instance variable of the same name
+  def name
+    return @name
+  end
+
+  # attr_reader :weapon create a method called weapon which reads the @weapon
+  attr_reader :weapon
+  # def weapon
+  #   @weapon
+  # end
+  attr_writer :weapon
+
+  def dead?
+    return @health <= 0
+  end
+
+  def take_damage(hit_points)
+    # @health = @health - 20
+    @health -= hit_points
+  end
+
+  # joe.attack(sergio)
+  # warrrior IS sergio
+  def attack(warrior)
+    # getting the HP of joe's weapon
+    hit_points = Warrior.weapons[@weapon]
+
+    warrior.take_damage(hit_points)
+  end
+
+  # CLASS method
+  # Warrior.weapons
+  def self.weapons
+    {
+      'sling shot' => 1,
+      'shiv' => 5,
+      'katana' => 10,
+      'battleaxe' => 15,
+      'lazer gun' => 20,
+      'napalm' => 25
+    }
+  end
+end
