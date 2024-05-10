60ea43f3581617c57edad07a471fadae94febba4
soufiane ezzine <ezzine.soufiane@gmail.com>
2024-05-10 11:26:18 +0900

    add session 2 code and slides

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,77 @@
 ### Controling Ruby Flow
+# This file is used to livecode each part during a live session
+# conditionals
+# if statement
+
+# expressions
+# arithmetic expression
+2 + 2 # 4
+3 - 1 # 2
+
+# variabele expression
+age = 32
+
+# boolean expression
+2 > 3 # greater than
+2 >= 3 # greater or equal
+3 < 2 # less than
+3 <= 2 # less or equal
+4 == 4 # equality operator
+4 != 4 # inequality operator
+
+age = 17
+
+if age >= 18
+  puts "yes you are allowed to drink"
+else
+  puts "Sorry come back when you are 18"
+end
+
+# Pseudo code
+# Welcome the user to our restaurant
+# Ask the user what time they are inquiring about
+# get user answer and store in a variable ( time )
+# compare the variable to opening hours of our restaurant
+# print the correct information
+
+puts "Welcome to Jiro Sushi 🍣"
+puts ''
+puts 'What time are you inquiring about?'
+print '>'
+
+# get the use input and convert it to an integer
+time = gets.chomp.to_i
+
+dinner = time >= 19 && time <= 22
+lunch = time >= 12 && time <= 15
+
+if time == 11
+  puts "The restaurant is opening soon"
+elsif dinner
+  puts "The restaurant is open for dinner"
+elsif lunch
+  puts "The restaurant is open for lunch"
+else
+  puts "Sorry the restaurant is closed"
+end
+
+# unless is_open
+#   puts "Sorry the restuarant is closed"
+# else
+#   puts "The restaurant is open"
+# end
+
+# Logical NOT ( ! ) operator
+# puts !true # false !true == false
+# puts !false # true !false == true
+
+# logical AND (&&) / OR ( || )
+
+# Logical AND
+# true && false && true
+# condition && contidion ... => all the sides of the expression have to be true ( for the expression to be true)
+
+# Logical OR
+# false || true || false
+# condition || condition ... => At least one of the sides of the expression has be true( for the expression to be true)
+
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,82 @@
 ### Looping with Ruby
+# This file is used to livecode each part during a live session
+# puts "Print me!"
+
+# Range: New data type
+(1..5) # sequence of numbers between 1 an 5 => 1 2 3 4 5 (5 is included)
+(1...5) # sequence of numbers bet ween 1 and 5 ( 5 is excluded)
+
+p (1..5).to_a
+p (1...5).to_a
+p ("a"..'g').to_a
+
+# for loop
+for a in (1..5)
+  puts "My number is #{a}"
+end
+
+# loop do
+count = 1
+
+loop do
+  puts "My number is #{count}"
+  count = count + 1
+
+  if count > 5
+    break
+  end
+end
+
+# Pseudocode
+# Price is right to illustrate a while loop
+
+# Greet the user
+# set a price for the product
+# ask the user to guess a price
+# store the user input into a variable
+# check is they guessed right
+# Keep asking until they get right
+
+puts 'Welcome The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+price_to_guess = rand(1..10)
+
+puts "Guess a price between 1 and 10!"
+print ">"
+
+users_guess = gets.chomp.to_i
+
+while price_to_guess != users_guess # did the user guess right
+  puts "Wrong Try again"
+  print ">"
+  users_guess = gets.chomp.to_i
+end
+
+puts "You guessed right the price was #{price_to_guess}"
+
+# unless <=> while ( loop )
+# if <=> until (loop)
+
+# Iterables
+# Array is an iterable
+# Range is an iterable
+
+students = ["Ali", "Jon", "Naomi", "Shin"]
+
+# for student in students
+#   puts "#{student} is attending the session today"
+# end
+
+# array methods
+# iterator .each
+# array.each do |element|
+  # repeat this
+# end
+
+students.each do |student|
+  puts "#{student} is attending the session today"
+end
+
+students.each { |student| puts "#{student} is attending the session today" } # one line version of .each
