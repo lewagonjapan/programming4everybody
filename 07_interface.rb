fe78346e6202a96d361297e2c59a8fe8ed961c4d
Douglas Berkley <douglasmberkley@gmail.com>
2024-05-21 20:28:10 +0900

    day 5 complete

--- 07_interface.rb
+++ 07_interface.rb
@@ -1,1 +1,56 @@
-### OOP Recap
+# we created the class Warrior here so we need to load it first
+require_relative '07_oop'
+
+# Communicating with the user
+# puts / gets.chomp
+
+puts "What's player one's name?"
+name_one = gets.chomp
+
+puts "What's player two's name?"
+name_two = gets.chomp
+
+player_one = Warrior.new(name_one, Warrior.weapons.keys.sample)
+player_two = Warrior.new(name_two, Warrior.weapons.keys.sample)
+
+
+# array[0]
+# hash['key']
+
+puts "*********************"
+puts "Welcome the Le Wagon Arena ⚔️"
+puts "*********************"
+puts "Battling today, we have #{player_one.name.capitalize} vs #{player_two.name.capitalize}"
+sleep(2) # a way to pause for 2 seconds
+puts "Fight!"
+
+# start the loop
+until player_one.dead? || player_two.dead?
+  puts "#{player_one.name} is attacking with a #{player_one.weapon}"
+  player_one.attack(player_two)
+  puts "#{player_two.name}'s health is #{player_two.health}."
+  sleep(2)
+
+  unless player_two.dead?
+    puts "#{player_two.name} is attacking with a #{player_two.weapon}"
+    player_two.attack(player_one)
+    puts "#{player_one.name}'s health is #{player_one.health}."
+    sleep(2)
+  end
+  puts ''
+
+  # how can i change player's weapon???
+  player_one.weapon = Warrior.weapons.keys.sample
+  player_two.weapon = Warrior.weapons.keys.sample
+end
+# end the loop
+
+# display the results
+if player_one.health > player_two.health
+  puts "#{player_one.name} is the champion!"
+elsif player_two.health > player_one.health
+  puts "#{player_two.name} is the champion!"
+else
+  puts 'Everyone died...'
+end
+# loop -> while / until
--- 07_oop.rb
+++ 07_oop.rb
@@ -1,1 +1,122 @@
 # OOP = OBJECT ORIENTED PROGRAMMING
+
+# Everything in Ruby is an object!
+# OOP = Data + Behavior
+
+# data -> information we want to store
+# behavior -> actions we want to run on that data (aka methods)
+
+# String
+'yuta' # data
+'yuta'.upcase # behavior for a string
+'yuta'.capitalize # behavior
+'yuta'.reverse # behavior
+
+# Prebuilt Objects
+String # .upcase, .capitalize
+Array  # .join, .first, .push
+Integer # .even? .odd? .positive?
+
+String # class    -> cake mould, makes the cale
+'yuta' # instance -> cake
+
+# naming conventions
+# methods/variables -> lower_snake_case
+# class -> UpperCamelCase
+
+# defing a method -> def
+def method_name
+end
+
+# defining a class -> class
+class ClassName
+end
+
+# Build a mini Terminal game to fight to the death
+# create a class for a Warrior
+# to create an instance of a warrior:
+# Warrior.new -> create an instance of a warrior AND triggers the method .initialize
+
+# data + behavior
+# what kind of data do we want for a warrior?
+class Warrior
+  # this gets triggered by .new
+  # set the initial data for my instance
+  def initialize(name, weapon)
+    # to store data to use later, we use @instance_variables
+    @name = name
+    @weapon = weapon
+    @health = 100
+  end
+
+  # by default, all methods are INSTANCE methods
+  # yuta.name # should return a string with the name
+  def name
+    # when we are instance INSTANCE methods, we have access to all of the instance variables
+    return @name
+  end
+
+  # attr_reader -> a shortcut to create a method that reads an instance variable of the same name
+  attr_reader :health, :name, :weapon
+  # this creates a method to be able to change the instance variable of the same name:
+  # yuta.weapon = 'sword'
+  attr_writer :weapon
+  # def health
+  #   @health
+  # end
+
+  # warrior.weapon
+  # def weapon
+  #   @weapon
+  # end
+
+  def take_damage(weapon_points)
+    # @health = @health - 20
+    @health -= weapon_points
+  end
+
+  # yuta.dead?
+  def dead?
+    @health <= 0
+  end
+
+  # yuta.attack(fabio)
+  def attack(warrior)
+    weapon_points = Warrior.weapons[@weapon]
+    warrior.take_damage(weapon_points)
+  end
+
+  # yuta.weapons    -> DONT WANT INSTANCE METHOD
+  # Warrior.weapons -> class method
+  # to create a class method, we need def self.method_name
+  def self.weapons
+    {
+      'gun' => 40,
+      'axe' => 30,
+      'sword' => 20,
+      'nunchucks' => 10
+    }
+  end
+end
+
+
+
+class User
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Home
+  def initialize(address)
+    @address = address
+  end
+end
+
+class Booking
+  def initialize(user, home, date)
+    @user = user
+    @home = home
+    @date = date
+  end
+end
