1bc158b505f7baba5639cf2b7148040538c8754f
soufiane ezzine <ezzine.soufiane@gmail.com>
2024-07-19 11:28:09 +0900

    session 2 code + slides

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,86 @@
 ### Controling Ruby Flow
+
+# Conditionals
+
+# if condition
+#   "this code is going to execute if the condition is met"
+# else
+#   "this code is going to execute if the condition is not met"
+# end
+
+# unless condition # opposite if an if statement
+#   "this code is going to execute if the condition is not met"
+# else
+#   "this code is going to execute if the condition is met"
+# end
+
+# Logical AND (&&) ## all the sides of the expression have to be true for the expression to be true
+true && true # true
+false && true # false
+# Logical OR (||) ## At least one of the sides of the expression has to be true for expression to be true
+true || false || false # true
+false || false || false # false
+true || true || true || false # true
+
+# Logial not ( ! )
+true == !false
+false == !true
+
+# In ruby everything exept false and nil is true
+
+age = 16
+
+if age >= 18
+  # puts "You can drink!"
+else
+  # puts "You can not drink! Sorry"
+end
+
+# expressions
+2 + 2 # arithmetic expression
+2 - 2
+
+x = 20 # variable
+y = x + 1
+
+# boolean expressions
+2 == 4 # boolean expression
+2 > 4
+2 < 4
+
+# different comparisons in ruby
+2 == 4 # equality operator ()
+2 != 4 # Inequality operator
+2 > 4 # Greater than
+2 >= 4 # Greater or equals
+2 < 4 # less than
+2 <= 4 # less or equals
+
+# live code
+# pseudo code
+
+# 1 - Welcome the user to our restaurant ok
+# 2 - Ask the user for a time ok
+# 3 - Get the user input and store it in a variable so i can reuse it ok
+# 4 - compare the user input to the opening hours of the restaurant ok
+# 5 - print the correct information ok
+
+puts "Welcome to Jiro Sushi 🍣"
+puts ''
+puts 'What time are you inquiring about?'
+print '> '
+
+time = gets.chomp.to_i
+
+evening = time >= 19 && time < 23
+noon = time >= 12 && time < 16
+
+if time == 11
+  puts "The restaurant is opening soon"
+elsif evening
+  puts "The resraurant is open for dinner"
+elsif noon
+  puts "The restaurant is open for lunch"
+else
+  puts "The restaurant is closed!"
+end
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,125 @@
 ### Looping with Ruby
+
+# puts "Print me"
+#
+# puts "Print me"
+# puts "Print me"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+
+# DRY Do not Repeat Yourself
+
+# new data type yaay
+# Range
+# puts (1..10).class # Range
+# p (1..10).to_a
+# p (1...10).to_a
+#
+# p ('a'..'z').to_a
+# p ('A'..'Z').to_a
+# p ('A'..'a').to_a # be careful
+
+# for loop
+for _ in 1..10
+  puts "Print me"
+end
+
+
+# loop do
+counter = 1 # initialize a counter
+
+loop do
+  # print my sentence to the terminal
+  puts "Print me #{counter}"
+  # add one to the counter
+  counter += 1
+
+  # if statement inside the loop
+  # checks if the coutner if greater than 10
+  if counter > 10
+    break
+  end
+end
+
+# times loop
+10.times do
+  puts "Print me"
+end
+
+
+# The game is right
+# 1 - greet the user ok
+# 2 - set a price to guess ok
+# 3 - ask the user to guess a price
+# 4 - keep asking the user to guess until / while the guess is right / not right
+# 5 - final message when the user guesses correctly
+
+puts 'Welcome The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+price_to_guess = rand(1..5)
+
+puts 'Guess a price between 1 and 5'
+
+user_guess = gets.chomp.to_i
+
+# until user_guess == price_to_guess
+#   puts "Try again"
+#   print "> "
+#   user_guess = gets.chomp.to_i
+# end
+
+while user_guess != price_to_guess
+  puts "Try again"
+  print "> "
+  user_guess = gets.chomp.to_i
+end
+
+puts "Congratulation you guessed right"
+puts "the price to guess was #{price_to_guess}$"
+
+
+# iterators / iterables
+# Array is an iterable
+# range is an iterable
+
+
+students = ["carl", "damien", "youssef"]
+
+# for student in students
+#   puts student
+# end
+
+# array.each do |element|
+#   do something
+# end
+
+students.each do |student|
+  puts "#{student} is a wonderful person!"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
