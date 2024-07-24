1e05f90edd364b3bd44c89bcb6817832ec8028c0
soufiane ezzine <ezzine.soufiane@gmail.com>
2024-07-24 10:45:20 +0900

    session 3 code + slides

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,124 @@
-
 ### Arrays
+
+students = ["carl", "lopez", "toby", "stamati", "stuart"]
+# indexes     0        1        2        3         4
+
+# CRUD
+# Basic operations that can be used to manipulate data
+
+# CREATE
+# shovel operator <<
+# array << element
+students << "youssef"
+
+# push method
+# array.push(element)
+students.push("damien")
+
+# READ
+# array[index]
+# read at specific index
+students[0]
+students[1]
+
+# read last element of an array
+students[-1] # return the last element of an array
+
+# first 3 elements
+# array[range]
+students[0..2]
+
+# find the the index of an element in an array
+# array.index(elemenet)
+students.index('youssef')
+students.index('soufiane')
+
+# UPDATE
+# array[index] = new_value
+students[0] = "mason"
+students[-1] = "soufiane"
+
+
+# DELETE
+# array.delete(element)
+students.delete("soufiane")
+
+# array.delete_at(index)
+students.delete_at(0)
+students
+
+
+# Iterators / Array methods
+# Methods built in ruby to make looping / iterating over arrays and working with them easy
+# There are many useful method (you can see the ruby documentations for all the available methods)
+
+students = ["carl", "lopez", "toby", "stamati", "stuart"]
+
+# .each
+# array.each do |name|
+#   do something
+# end
+
+# Block do ... end
+# method without a name (anonymous method)
+
+# multiple line syntax
+students.each do |s|
+  # puts "#{s} is a good student!!!"
+end
+
+# single line syntax
+students.each { |student| "#{student} is a good student!!!" }
+
+# .each_with_index
+students.each_with_index do |student, index|
+  # puts "#{index + 1} - #{student.capitalize}"
+end
+
+# .map
+numbers = [2, 3, 5, 6]
+
+
+# result = []
+# numbers.each do |number|
+#   result << number + 1
+# end
+# p result
+squares = numbers.map do |number|
+  number * number
+end
+
+# p squares
+
+
+# .select
+s_students = students.select do |student|
+  student[0] == 's'
+end
+s_students
+
+# .count
+p students.count
+
+# s_names = students.count do |student|
+#   student[0] == 's'
+#   end
+
+s_names = students.count { |student| student[0] == 's'}
+
+p s_names
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,81 @@
-
 ### Methods
+
+# Methods (function, precedures) a way to store a chunk of code ( some logic ) in order to be able to reuse
+# why methods ?
+# DRY
+# break down complexe code into more managable, reusable chunks
+
+# first_name = "damien"
+# last_name = "ezzine"
+#
+# cap_fname = first_name.capitalize
+# cap_lname = last_name.capitalize
+#
+# full_name = "#{cap_fname} #{cap_lname}"
+# full_name
+
+# a function is defined using the def keyword and closed with the end keyword
+# a function may or may not take arguments
+# a function may or may not return a value
+
+# defined a function
+# when defining we pass *arguments*
+# return keyword is optional
+# a method will always return the last line of codes
+def capitalize_name(f, l)
+  cap_fname = f.capitalize
+  cap_lname = l.capitalize
+
+  # return "#{cap_fname} #{cap_lname}"
+  "#{cap_fname} #{cap_lname}"
+end
+
+# Invoke / use / call the function
+# when using the function we pass *parameters*
+
+soufiane = capitalize_name("soufiane", "ezzine")
+
+soufiane
+
+# METHODS naming convesion
+# method names should always be lower_snake_case
+
+# methods ending with a ? .odd? .even?
+#  # should always return a boolean
+
+def is_even?(number)
+  number % 2 == 0
+end
+
+p is_even?(3)
+p is_even?(10)
+p is_even?(0)
+
+# methods ending with a ! .capitalize! .map! ( Dangerous methods )
+## Mutate or change the data that is used with them or they are called on
+def add_bangs!(str)
+  str.replace(str + "!!!")
+end
+
+greeting = "Hello"
+p greeting
+add_bangs!(greeting)
+p greeting
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
