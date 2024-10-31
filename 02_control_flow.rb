b249b0fe627f156275f8738484c0ac5737170842
soufiane ezzine <ezzine.soufiane@gmail.com>
2024-10-31 21:03:23 +0900

    session 2 code

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,75 @@
 ### Controling Ruby Flow
+# Conditionals
+# if condition
+#   this code is going to run if the condition is true
+# else
+#   this code is going to run if the condition is not true
+# end
+#
+age = 30
+
+# if age > 18
+#   puts "You are old enough!"
+# else
+#   puts "You are not old enough come back when you are 18!"
+# end
+
+# Expressions
+# Arithmetic expressions
+1 + 2
+1 - 1
+# variable expression
+age = 13
+# Boolean expression
+12 == 18 # equality operator ( two == )
+12 != 18 # not equal
+12 > 18 # greather than
+12 < 18 # less than
+12 >= 18 # geater than or equal
+12 <= 18 # less than or equal
+
+# Pseudocode
+# Program to to check is a restaurant is open or not
+
+# 1- Welcome the user to the restaurant app
+# 2- Ask the user what time are they asking about
+# 3- Get the user answer / input and store in a variable
+# 4- Compare the variable to the defined times
+# 5- Print the correct information / answer to the user
+
+puts "Welcome to Jiro Sushi 🍣"
+puts ''
+puts 'What time are you inquiring about?'
+print '> '
+
+time = gets.chomp.to_i
+dinner = time >= 19 && time < 22
+lunch = time >= 12 && time < 16
+
+if time == 11
+  puts "The restaurant is opening soon"
+elsif dinner
+  puts "The restaurant is open for dinner"
+elsif lunch
+  puts "The restaurant is open for lunch"
+else
+  puts "The restaurant is closed!"
+end
+
+# Logical and (&&) # In order for an expression with logical and to be true all the sides of the expression have to be true
+true && true && false
+# Logical Or (||) # In order fot an expression with logical OR to be true at least one of the sides have to be true
+true || false || false
+
+#  Logical NOT ( ! )
+!true == false
+!false == true
+
+age = 30
+condition = age > 18
+
+if !condition
+  puts "you are not old enough"
+else
+  puts "you are old enough"
+end
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,96 @@
 ### Looping with Ruby
+
+# looping
+# is a way to automatically repeat a chunk of code a set number of times
+
+# New Data type (range)
+# A range represents an interval set of values beginning and ending bet ween the ..
+# puts (1..10).class
+# p (1..10).to_a # creates an array from 1 to 10 ( range ) including the last element (..)
+# p (1...10).to_a # array from 1 to 9 ( ... )
+#
+# p ('a'..'z').to_a
+# p ('a'..'z').to_a
+# p ('A'..'z').to_a
+
+# Each time the loop repeats it self ( we call that an iteration )
+
+# for loop
+for number in 1..15
+  puts "The number is %02d" % number
+end
+
+# loop do
+counter = 1 # initialize with the value 0
+loop do
+  # puts "#{counter} Print me!"
+  counter += 1 # counter = counter + 1
+  if counter > 10
+    break
+  end
+end
+
+# x.times
+10.times do
+  # puts "Print me!"
+end
+
+# Livecode
+# Pseudocode
+
+# Greet the user the app
+# Set the price of the product
+# Ask the user to guess a price between 1 - 5$
+# Keep asking until the user guesses right
+# Final message when the user wins
+
+puts 'Welcome The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+price_to_guess = rand(1..5)
+
+puts "Guess a price between 1 and 5!"
+print "> "
+user_guess = gets.chomp.to_i
+
+# until price_to_guess == user_guess # until the condition is met
+while price_to_guess != user_guess # while the condition is not met
+  #   keep asking the user whill the price to guess is not the users guess
+  puts "try again!"
+  print "> "
+  user_guess = gets.chomp.to_i
+end
+
+puts "Congratulations you guessed it right"
+puts "The price to guess was #{price_to_guess}!"
+
+# while loop
+# like an if statement take a condition
+# the loop will keep iterating / running until the condition is false
+
+# until loop
+# loop will keep iterating / runnin until the condition is true
+
+# Iterators / iterables
+# iterator is a tool we use to loop over list of data
+# iterable is a list of data ( array , range , hash )
+
+
+# for student in students
+#   # puts "#{student} is a student at le WAGON!"
+# end
+
+# .each do end
+# iterable.each do
+# code that is going to be repeated
+# end
+
+students = ['Amanda', 'Sandrine', 'Patrick']
+students.each do |student|
+  # puts "Hello #{student}!"
+  # puts "#{student} is a student at le wagon"
+end
+
+# one line version of the for each
+# students.each { |student|  puts "#{student} is a student at le wagon" }
