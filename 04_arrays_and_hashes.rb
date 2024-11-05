d98d54e5c9aab433a4001819e6c06e537c3bb91b
soufiane ezzine <ezzine.soufiane@gmail.com>
2024-11-06 00:25:27 +0900

    session 3 code and slides

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,147 @@
 
 ### Arrays
+[].class # Array
+[1, 2, 3] # Array of numbers
+["student1", "student2", "student3"] # Array of strings
+
+students = ["Sandrine", "Taka", "Amanda", "Carl", "Marina"]
+# index         0          1        2        3        4
+# index        -5         -4       -3       -2       -1
+
+# CRUD operation on arrays
+
+# Create / Add to array
+[] # Array litteral to create and empty array
+
+# shovel operator <<
+# array << element
+students << "Monika"
+
+# array.push(element)
+students.push("Hermione")
+
+# array.unshift(element) adds element at the beginning
+students.unshift("soufiane")
+
+puts students
+
+# Read
+# array[index]
+# read the first elemenet of an array
+puts students[0]
+#
+# last element of an array
+puts students[-1]
+
+# get the index of an element
+# array.index(element)
+puts students.index("Taka")
+
+# get slice of my array
+# array[range]
+puts students[0, 2]
+puts students[0..2]
+
+# Read an element outside the range
+p students[10] # nil
+p students.index("soufiane")
+
+students = ["Sandrine", "Taka", "Amanda", "Carl", "Marina"]
+# Update
+# array[index] = new_value
+students[0] = "Owen"
+# students[-1] = "soufiane"
+
+# Delete
+# array.delete(element)
+students.delete("Owen")
+
+# array.detele_at(index)
+students.delete_at(1)
+# puts students
+
+
+# Array Methods
+students = ["Sandrine", "Taka", "Amanda", "Carl", "Marina", "Mike"]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# .each
+students.each do |student|
+  puts student
+end
+
+total = 0
+numbers.each do |number|
+  total += number
+end
+
+puts total
+
+# .sum returs the sum of an array of integers / floats
+# puts numbers.sum
+
+# .map
+
+# to create an new array using .each
+upcased = []
+students.each do |student|
+  upcased.push(student.upcase)
+end
+p upcased
+
+# .map allows me to create a new array using the returned value of the block
+upcased = students.map do |student|
+  student.upcase
+end
+
+p students
+p upcased
+
+# .select filters the array and returns a new array with only
+# the elements that return true from the provided condition
+m_students = students.select do |student|
+  student[0] == "M" # Condition returns a boolean
+end
+
+n_num = numbers.select { |num| num >= 5 }
+p n_num
+
+p m_students
+
+# Block
+# defined with a do ... end
+# a block may or may not be defined with parameters
+# a block will "return" the last line
+# A block might be tought of as method without a name that exists only for the purpose of the
+# method
+
+# Livecode
+# Disclaimer /!\ this is and advanced live
+
+# Building a scraper
+
+# Pseudocode
+# Be able to open a webpage ( download the html file )
+# Understand the structure of the HTML file
+# Search fot the elements that I am targeting in the page
+# Extract those elements
+
+# Import a gem / package to be able to open a webpage
+require 'open-uri'
+
+# import a gem / package to parse and read the html
+require 'nokogiri'
+
+# Open rakuten page and read the html
+html = URI.open("https://search.rakuten.co.jp/search/mall/pillow/").read
+
+# Parse the html to  ruby objects
+parsed_html = Nokogiri::HTML(html)
+
+# Search the parsed html for all the elements that have the class ".price--OX_YW"
+prices = parsed_html.search(".price--OX_YW").map do |price|
+  price.text.strip
+end
+
+p prices
+
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,75 @@
 
 ### Methods
+
+# Variable is a way to store data
+# Method (function) is a way to store logic
+
+# Why ?
+# DRY: Do not Repeat Yourself
+# Breakdown a bigger problem into smaller chunks ( code problem )
+
+first_name = 'soufiane'
+last_name = 'ezzine'
+
+cap_fname = first_name.capitalize
+cap_lname = last_name.capitalize
+
+full_name = "#{cap_fname} #{cap_lname}"
+
+puts full_name
+
+# Remember !!!
+# a Method is defined with the def keyword and closed with the end keyword
+# a method may or may note return a value
+# a method will ALWAYS return the last line of code OR the line with the return keyword
+# a mehtod may or may not be defined with *parameters*, the order or parameters is important
+
+# Naming convetion
+# - lower snake case ( like variables)
+# - methods ending with ? will (almost) alway return a boolean
+# - methods ending with ! Destructive methods ( ! Dangerous methods )
+
+# defined method
+# def print_my_name(fname, lname="generic")
+def print_my_name(fname, lname)
+  # body of the method ( logic )
+  cap_fname = fname.capitalize
+  cap_lname = lname.capitalize
+
+  "#{cap_fname} #{cap_lname}"
+end
+
+string = "something"
+# puts string
+
+# we called / used / invoked they method
+# We call methods using *arguments*
+
+puts print_my_name("soufiane", "ezzine")
+puts print_my_name("marina", "musaeva")
+puts print_my_name("amanda", "smith")
+
+# methods with ?
+# puts 5.odd?
+# puts 5.even?
+
+def is_even?(number)
+  number % 2 == 0
+end
+
+# puts is_even?(20) # true
+# puts odd?(1) # false
+
+
+# methods with !
+
+string = "soufiane"
+puts string # soufiane
+
+string.upcase
+puts string # soufiane
+
+string.upcase!
+puts string # SOUFIANE
+
+string # UPCASED NAME for the rest of my program
