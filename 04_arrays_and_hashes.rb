e0b0e7e4b1c6b3bb2a6fd7fe2d4e916d1dd63fa1
Sylvain Pierre <sylvain.pierre@officience.com>
2025-01-22 08:48:02 +0900

    session 3 code

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,103 @@
 
 ### Arrays
+
+students = ["carl", "cassiano", "antonio", "badre"]
+# indexes     0          1          2         3
+# reverse.   -4         -3         -2        -1
+
+# CRUD operations
+# Are the basic operations that can be applied to data
+
+# Create
+arr = []
+# array << element ( shovel operator )
+students << "ryan"
+# array.push(element) adds at the end
+students.push("ryle")
+# array.unshift(element) add at the beginng
+# students.unshift("soufiane")
+
+# Read
+# array[index]
+students[0] # gets first element
+students[-1] # get last element
+students[0..2] # get the 3 first elements
+
+# get the index of an element
+# array.index(element)
+students.index("ryan") # returns index of the element
+
+# IF I TRY TO READ AN ELEMENT THAT DOES NOT EXIST
+students.index("soufiane") # nil
+students[10] # nil
+
+# Update
+# array[index] = new_value
+# students[0] = "max"
+# students[-1] = 'soufiane'
+
+# Delete
+# array.delete(element)
+students.delete("carl")
+# array.delete_at(index)
+students.delete_at(0)
+# student.shift # removes from the begining
+students.shift
+students
+
+
+# iterators and blocks
+# do ... end is called a block
+
+students = ["carl", "cassiano", "antonio", "badre"]
+# students = ["Carl!", "Cassiano!", "Antonio!", "Badre!"]
+
+#.each
+# array.each do |element|
+  # do something
+# end
+
+its_return = students.each do |student|
+  # puts "#{student.capitalize}"
+end
+# p its_return
+
+# one line syntax
+# students.each { |student| puts "#{student.capitalize}"}
+
+# .each_with_index
+students.each_with_index do |student, index|
+  # puts "#{index + 1} - #{student.capitalize}"
+end
+
+# I want to create a new array using elements from the origina array
+# transform
+numbers = [1, 3, 4, 6, 7]
+
+# doubles = []
+# numbers.each do |number|
+#   doubles << number * number
+# end
+# p numbers
+# p doubles
+
+# .map
+doubles = numbers.map do |number|
+  number * number
+end
+# p doubles
+
+names = students.map {|s| "#{s.capitalize}!" }
+# p names
+
+# .select
+c_name = students.select do |student|
+  student[0] != "c"
+end
+
+# .count
+p students.count
+c_names = students.count do |student|
+  student[0] == "c"
+end
+p c_names
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,66 @@
-
 ### Methods
+# methods
+# variabales allow us to store data ( to reuse it later )
+# Methods allow us to store logic ( chunck of code ) to be reused later
+# Methods / functions / procedures
+
+# Why?
+# DRY (stay)
+# Do not Repeat Yourself
+# To break down a larger problem into smaller pieces
+
+# A method is define with the def...end keyword
+# A method may or may not take parameters
+# The order or parameters matters
+# The return is optional
+# The last line will alway be returned
+
+
+
+first_name = "soufiane"
+last_name = "ezzine"
+
+cap_fname = first_name.capitalize
+cap_lname = last_name.capitalize
+
+full_name = "#{cap_fname} #{cap_lname}"
+
+# We defined a method ( I define parameters )
+def beautiful_name(fname, lname)
+  cap_fname = fname.capitalize
+  cap_lname = lname.capitalize
+
+  "#{cap_fname} #{cap_lname}"
+end
+
+# We call / use the method ( with argurments)
+# soufiane = beautiful_name("ezzine", "soufiane")
+# p soufiane
+beautiful_name("ezzine", "soufiane")
+beautiful_name("ryan", "ward")
+beautiful_name("crystal", "arevalo")
+beautiful_name("jester", "parez")
+
+# Naming convention
+# A methods name ( like variables ) should be lower_snake_case
+# Methods that end with a "?" *should* return a boolean
+
+
+4.even?
+def is_even?(number)
+  # % modulus operator / remainder
+  number % 2 == 0
+end
+
+# p is_even?(4)
+# p is_even?(5)
+
+# Methods that end with a "!" is a *destructive* methods (/!\) dangerous
+def add_bang!(string)
+  string.replace(string + "!") # hello => hello!
+end
+
+my_string = "Hello"
+p my_string
+add_bang!(my_string)
+p my_string
