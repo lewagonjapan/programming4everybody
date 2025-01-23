2e42645b395857895c58877d394d862668221a98
soufiane ezzine <ezzine.soufiane@gmail.com>
2025-01-23 20:51:58 +0900

    session 2 code

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,100 @@
 ### Controling Ruby Flow
+# Conditionals
+# if condition
+#   # some code that is going to run if the condition is true
+# else
+#   # some code that is going to run if the condition is not true
+# end
+
+# unless condition
+#   # code that is going to run if the condition is false ( not true )
+# else
+#   # code that is going to run if the conditiono is true
+# end
+
+# what is a condition
+# Any ruby expression that return a boolean
+# arithmetic expressions
+# 2 + 2
+# 4 - 5
+# varibale expression
+# age = 20
+# Boolean expression
+# ( == ) is the equality operator
+# ( = ) the assignment operator
+2 == 4 # is equal
+2 != 4 # inequality opertator
+2 > 4 # greater than
+3 < 7 # lower than
+3 >= 5 # greater than or equal
+5 <= 5 # lower than or equal
+
+# ( ! ) Not operator
+true == true # true
+false == false # true
+
+!true == false # true
+!false == true # true
+
+# logical AND ( && )
+number = 3
+number > 2 # true
+number == 3 # true
+
+# for expression with the logical AND (&&)
+# all sides of the expression have to be true
+(number > 2 && number == 3 && number > 5) # true
+# true && true => true
+# true && true && true && true => true
+# true && false && true && true
+
+# logical OR ( || )
+# for expression with the logical OR (||)
+# at least one side of the expression has to be true
+
+number = 10
+(number == 10 || number == 0) # true
+# true || true || true => true
+# false || false || false || true => true
+
+
+
+# age = 17
+#
+# if age >= 18
+#   puts "You can vote!"
+# else
+#   puts "You can not vote!"
+# end
+
+# pseudocode
+# Program to check if a restaurant is open or not
+
+# 1- Greet the user to our application
+# 2- Ask the user for the time they are inquiring about
+# 3- Store the user input / answer in a variable
+# 4- compare the users input with the opening hours of the restaurant
+# 5- Print the correct information
+
+puts "Welcome to Jiro Sushi 🍣"
+puts " "
+puts 'What time are you inquiring about?'
+print '> '
+
+time = gets.chomp.to_i
+lunch = time > 12 && time <= 16
+dinner = time > 19 && time <= 23
+
+# open = lunch || dinner
+# open = (time > 12 && time <= 16) || (time > 19 && time <= 23)
+
+# unless !(time > 12)
+if time == 11
+  puts "The restaurant is opening soon"
+elsif lunch
+  puts "The restaurant is open for lunch"
+elsif dinner
+  puts "The restaurant is open for dinner"
+else
+  puts "The restaurant is closed :("
+end
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,86 @@
 ### Looping with Ruby
+
+# Repeating a set of instructions ( some block of code ) a set number of times
+
+##### for loop
+students = ["carl", "korina", "maria", "jester"]
+
+for student in students # has to be a list of things / elements
+  # puts "#{student} is attending today's session"
+end
+
+# Range ( new datatype yaaay)
+
+# puts (0..10).class # range
+# p (0..10).to_a # a range between 0 and 10 ( 10 included)
+# p (0...10).to_a # a range between 0 and 10  ( 10 exluded )
+
+# p ('a'..'z').to_a
+# p ('A'..'z').to_a
+
+for x in (0..10)
+  # puts "My number is #{x}"
+end
+
+##### loop do
+counter = 1
+loop do
+  # puts "My number is #{counter}"
+  counter += 1
+
+  if counter > 10
+    break
+  end
+end
+
+###### x.times
+10.times do
+  # puts "print me!"
+end
+
+# pseudocode
+# The price right wit while loop / until loop
+
+# 1- Greet the user to the app
+# 2- Set the price to guess price_to_guess
+# 3- Ask the user for a price ( user_guess) store it in a variable
+# 4- Compare the price_to_guess to the user_guess
+# 5- Print a final message when the user guesses correctly
+
+# Keep repeating until the user guesses correctly
+
+puts 'Welcome The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+price_to_guess = rand(1..5)
+
+puts 'Guess a price between 1 and 5'
+
+user_guess = gets.chomp.to_i
+
+# while user_guess != price_to_guess
+until user_guess == price_to_guess
+  puts "try again!"
+  print "> "
+
+  user_guess = gets.chomp.to_i
+end
+
+puts ''
+puts 'Congrats! You guessed the right price'
+puts "The price to guess was $#{price_to_guess}"
+
+students = ["carl", "korina", "maria", "jester"]
+for student in students # has to be a list of things / elements
+  # puts "#{student} is attending today's session"
+end
+
+# .each
+students.each do |student|
+  # puts "#{student} is attending today's session"
+end
+
+# one line .each
+students.each { |student| puts "#{student} is attending today's session"}
+
