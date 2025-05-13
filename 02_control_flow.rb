58ec56fca0b815d2b2e4aee3266def639fde9ef5
soufiane ezzine <ezzine.soufiane@gmail.com>
2025-05-13 23:45:07 +0900

    session 2 code \

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,111 @@
 ### Controling Ruby Flow
+
+# Theory
+# gets.chom will always return a string
+
+# if condition
+#   Some code that is going to run if the condition is true
+# else
+#  some code that is going to run if the condition is false
+# end
+
+# unless !condition
+#   some code that is going to run if the condition is false
+# else
+#   some code that is going to run if the condition is true
+# end
+
+# Condition
+# an expression that returns / evaluates to a boolean
+
+# Expression
+# Arithmetic expressions
+1 + 2 # returns an integer
+3 / 3
+# Assignement expression
+age = 18
+# literal expressions
+"string"
+# method expressions
+
+
+# Boolean expressions
+# when comparing values, data types, variables
+
+1 > 2 # return false # greater than operator >
+2 < 1 # return false # less than operator <
+3 >= 3 # return true # greater or equals than operator
+5 <= 4 # return false # less than or equal operat
+4 == 4 # return true # Equality operator
+5 != 5 # return false # Not equal
+
+# Logical AND => &&
+# in order for the expression to be true, ALL THE SIDES OF THE EXPRESSION HAVE TO BE TRUE
+true && true && true # true
+false && true && true # false
+
+# Logical OR => ||
+# in order for the expression to be true, AT LEAST ONE SIDE OF THE EXPRESSION HAS TO BE TRUE
+false || false || true # true
+false || false || false # false
+# logical NOT ( ! )
+
+age == 32 # equality operator
+age != 32 # Not equal operator
+
+true == !true # false
+false == !false
+
+# pseudo code
+# 1- Greet the user to my super restaurants application
+# 2- Ask the user for a time (what time are they enquiring about)
+# 3- Store the time in a variable called time
+# 4- Compare the variable to the opening hours of my restaurant
+# 5- Print the correct information to the user
+
+puts "Welcome to Jiro Sushi 🍣"
+puts ''
+puts 'What time are you inquiring about?'
+print '> '
+
+time = gets.chomp.to_i
+
+lunch = time >= 12 && time <= 16
+dinner = time >= 20 && time <= 23
+
+if lunch
+  puts "The restaurant is open for lunch!"
+elsif time == 11
+  puts "The restaurant is opening soon!"
+elsif dinner
+  puts "The restaurant is open for dinner!"
+else
+  puts "The restaurant is closed :("
+end
+
+# if password is 10 char long
+# run this logic first
+# if password contains numbers
+# run this logic as well
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,141 @@
 ### Looping with Ruby
+# puts "Print me!"
+
+# puts "Print me for the boss!"
+# puts "Print me for the boss!"
+# puts "Print me for the boss!"
+# puts "Print me for the boss!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+# puts "Print me!"
+
+# DRY
+# Do not Repeat Yourself
+#
+# looping
+# Repeating a piece of code a set number of times
+
+# New Datatype Range
+# represents values within a certain range ( sequencial )
+puts (1..10).class
+p (1..10).to_a # bertween 1 an 10 inlusive (the 10)
+p (1...10).to_a # between 1 and 10 exclusive (the 10)
+
+p ('A'..'z').to_a # Look for the ASCII table
+p ('A'..'Z').to_a
+
+# for loop
+#
+for n in 1..10
+  puts n
+end
+#
+students = ["camila", "leanardo", "giada", "matt"]
+
+for student in students
+  puts "#{student} is a great student!"
+end
+
+# loop do
+counter = 1
+loop do
+  puts counter # 10
+  counter += 1
+
+  if counter > 10
+    break
+  end
+end
+
+# x.times do
+10.times do
+  puts "I love coding!"
+end
+
+# live code
+# 1- Greet the user to our application ok
+# 2- Set the price to guess
+# 3- Ask the user to guess the price
+# 4- Compare the user's guess with the price to guess
+#
+# 5- keep asking until the user guesses right
+# 6- Congratulate the user
+
+puts 'Welcome The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+price_to_guess = rand(1..10)
+
+puts "Guess a price between 1 and 10"
+
+users_guess = gets.chomp.to_i # 1
+
+until users_guess == price_to_guess
+  puts "Try again!"
+  print "> "
+  users_guess = gets.chomp.to_i
+end
+
+puts "You guessed it right!"
+puts "The price to guess was #{price_to_guess}"
+
+# while true
+#   puts "print line"
+# end
+
+
+# iterators / iterable
+# iterable => Is a datatype I can iterate over ( loop over )
+# iterator => A tool provided by ruby to iterate over lists ( iterables )
+
+students = ["camila", "leanardo", "giada", "matt"]
+#.each
+
+# for student in students
+#   puts student
+# end
+
+# multiline syntax
+p students.each do |student|
+  puts "#{student} is a great student!"
+end
+
+# single liner (single line syntax)
+students.each { |student| puts "#{student} says hello to you!" }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
