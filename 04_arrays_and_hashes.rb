eee3dfb00d7ace119014651ce5a8094ffa01baa0
soufiane ezzine <ezzine.soufiane@gmail.com>
2025-05-16 00:22:10 +0900

    session 3 code and slides

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,159 @@
-
 ### Arrays
+### === Recap / New stuffs ===
+
+# array is delimited with square brackets
+[].class
+
+# Arrays are a collection / list of elements
+[1, 2, 3] #  Array of integers
+["luis", "youssef", "paul"] # array of strings
+
+[1, "sylvain", true, 0.4]
+[[1, 2, 3], [4, 5 , 6], [7, 8, 9]]
+
+# each eleemnt of the array has an address ( index )
+students =       ["luis", "youssef", "paul", "diana"]
+# index             0         1        2        3
+# reverse indexes  -4        -3       -2       -1
+
+# CRUD operations
+# 4 basic operations that can do to data
+students = ["luis", "youssef", "paul", "diana"]
+
+# Read
+# array[index]
+students[0]
+
+# get the first element (s) in an array
+students.first(2)
+
+# get last element in my array
+students[-2]
+students.last
+
+# get second and third element using a reange
+students[1..2]
+
+# get the index of an element in an array
+# array.index(element)
+students.index("diana")
+
+
+# if you try to read an element with an index that does not exist
+students[10] # nil
+students.index("soufiane") # nil
+
+# Create
+# [...]
+students = ["luis", "youssef", "paul", "diana"]
+# shovel operator
+# array << element
+students << "alonzo"
+
+# push method
+# array.push(element)
+students.push("lan")
+
+# array.unshift # add at the beginning of an array
+students.unshift("soufiane")
+
+# Update
+# array[index] = new_value
+students[-1] = "jennifer"
+students[0] = "jennifer"
+
+# Delete
+# array.delete(element)
+#.delete will delete all the instances of the element
+students.delete("youssef")
+
+# students.delete_at(index)
+students.delete_at(0)
+
+# array.shift
+students.shift
+# p students
+
+# CRUD for arrays is super important, you should know them by heart
+
+
+# Iterators
+
+students = ["luis", "youssef", "paul", "diana", "giada"]
+
+# for student in students
+#   puts "#{student} is taking P4E!"
+# end
+
+# .each
+# array.each do |element|
+# end
+
+# if I want to loop over an array of element and do something
+students.each do |s|
+  puts "#{s} is taking P4E!"
+end
+
+# .each_with_index
+# if I want to loop over an array of elements and get access to their index
+students.each_with_index do |student, index|
+  puts "#{index + 1}) #{student} is taking P4E!"
+end
+
+p grades = [2, 3, 6, 4, 9]
+
+new_grades = []
+grades.each do |grade|
+  new_grades.push(grade + 1)
+end
+
+# p new_grades
+
+# if I want to create a new array from another array and transform the elements in some whay
+# .map
+
+new_grades = grades.map do |grade|
+  grade * 2
+end
+
+p new_grades
+
+students = ["luis", "youssef", "paul", "diana", "giada", "donna" ]
+captalized = students.map do |student|
+  student.capitalize
+  student[0]
+end
+p captalized
+
+
+#.select
+# Filters or selects elements from an array that meets the condition defined in the block
+d_names = students.select do |student|
+  student[0] == "d"
+  # student.chars.first == "p"
+end
+
+p d_names
+
+# .count
+# If count is not given a block it will return the length of the array
+p counter = students.count
+
+# If given a block, count will count the element in the array that meet the condition defined in the block
+counter = students.count do |student|
+  student[0] == "d"
+end
+
+# p counter
+
+# blocks
+grades.map do |grade|
+  grade * 2
+end
+
+# - Everything between the do ... end is called a block
+# - *grade* is called a block parameter ( in this specific example )
+# A block is a way to pass code to a method
+
+## One line block syntax
+grades.map { |grade| grade * 2 }
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,53 @@
 
 ### Methods
+# Called methods, functions or procedures in other programming languages
+# A method is defined with a def keyword and closed with end keyword
+# When defining a method I may pass *parameters*
+# When calling a metghid I need to pass *arguments* ( if the method takes arguments)
+# .method and def method() are both methods
+# Methods may or may not return values, if its the case it may have a return statement
+# The return keyword is optional, the method will alway return the last line's value
+# The data type of arguments should match the parameter's use
+# The order of arguments should match the order of paramters
+
+# *Define* a method to display team members
+def display_team(team_array, team_name)
+  team_array.each do |member|
+    puts "#{member} is a #{team_name} member!"
+  end
+end
+
+# use / *invoke* / *call* the method
+team1 = ["luis", "youssef", "paul"]
+team2 = ["diana", "giada", "donna"]
+
+display_team(team1, "The Coders")
+puts " "
+display_team(team2, "The Players")
+
+
+# Naming convetion
+# Method names are always ( like variable ) lower_snake_case
+# - some methods will end with ? => I know that this method will always ( hopefully ) return a boolean
+# - some methods will end with ! => Destructive methods ( dangerous methods )
+
+# ? methods
+5.odd?
+def is_even?(integer)
+  integer % 2 == 0
+end
+
+# p 5.even?
+# p is_even?(6)
+
+# ! methods
+
+team1 = ["luis", "youssef", "paul"]
+p team1
+captitalized = team1.map! do |student|
+  student.capitalize
+  puts ""
+end
+
+p captitalized
+p team1 # this array is now changed compared to its original form
