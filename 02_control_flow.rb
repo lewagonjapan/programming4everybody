180b8f8b98d8fa8db6447c0c08d274287fee090d
soufiane ezzine <ezzine.soufiane@gmail.com>
2025-08-01 01:23:42 +0900

    code session 2

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,124 @@
 ### Controling Ruby Flow
+
+age = 17
+
+if age >= 20
+  puts "You are old enough to drinK!"
+else
+  puts "Sorry come back when you are old enough"
+end
+
+# if condition
+# run this code is the condition is true
+# else
+# run this code is the condition is false
+# end
+
+# Conditions
+# Are expressions that evaluate to a boolean
+# an expression is any ruby code that returns a value ( most of written ruby is an expression )
+#
+# Arithmetic expressions
+1 + 2 #=> 3
+2 / 3
+
+# Assignement expressions
+age = 30
+# y = x + 2
+
+# Literal expressions
+# methods
+
+# boolean expressions
+2 == 4 # == equality operator ( not mix up with the assignement operator =)
+2 > 3 # > greater than
+3 >= 3 # Greater or equals
+2 < 3 # less than
+2 <= 3 # less than or equals
+
+# live code pseudocode
+
+# We have a restaurant
+# Write a program to check if a restaurant is open at a given hour
+
+# 1- Welcome the user to our super app
+# 2- Ask the user what time they are inquiring about
+# 3- Get the user answer / time and store it in a variable
+# 4- Compare the users input with the opening hours of our restaurant
+# 5- Print the correct information
+
+puts "Welcome to Jiro Sushi 🍣"
+puts ''
+puts 'What time are you inquiring about?'
+print '> '
+
+time = gets.chomp.to_i # get.chomp will always return a string (to_i to trun it into an integer)
+
+if time == 11
+  puts "The restaurant is opening soon"
+elsif time >= 20 && time < 23
+  puts "The restaurant is open for dinner!"
+elsif time >= 12 && time < 16
+  puts "The restaurant is open for lunch!"
+else
+  puts "The restaurant is closed!"
+end
+
+## Boolean logic logical AND / logical OR
+# Logical AND (&&) => The expression will return true only
+# if ALL the combining expressions are true
+true && true && false && true # false
+
+true && true # true
+false && false # fasle
+true && false # fale
+false && true # false
+
+# Logical OR ( || ) => The expression will return true
+# if at least one  of the combining expression is true
+
+false || false # false
+true || true # true
+false || true # true
+true || false # true
+
+# logical NOT ( ! )
+!true == false
+!false == true
+
+# inequality operator
+# time != 11 # boolean
+
+# unless condition
+#   # run if the condition is false
+# else
+#   # run if the condition is true
+# end
+
+
+# time = 14
+#
+# case time
+# when 11, 10
+#   puts "time is eleven or 10"
+# when 12..15
+#   puts "time is between 12 and 15"
+# else
+#   puts "time is not 11 nor 12"
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,101 @@
 ### Looping with Ruby
+
+# puts "print me!!!!"
+# puts "print me!"
+# puts "print me!"
+# puts "print me!"
+# puts "print me!"
+# puts "print me!"
+# puts "print me!"
+# puts "print me!"
+# puts "print me!"
+# puts "print me!"
+# puts "print me!"
+
+# we take advantage of the power of computers
+# DRY Do not Repeat Yourself
+
+# new data type
+# RANGE
+(1..10).class
+(1..10).to_a
+# with two dots .. -> between 1 and 10 ( 10 inclusive)
+(1...10).to_a
+# with three dots ... -> between 1 and 10 ( 10 excluded)
+('a' .. 'z').to_a
+('A'..'C').to_a
+
+
+# for loop
+# x is just a variable
+for x in (1..10).to_a
+  # puts "My number is #{x}"
+  # puts "print me!"
+end
+
+
+# loop do
+counter = 1
+
+loop do
+  # puts "This is iteration number ##{counter}"
+  counter += 1
+  # counter = counter + 1
+  if counter > 10
+    break # break out of the loop
+  end
+end
+
+# x.times
+
+10.times do
+  # puts "Print me!"
+end
+
+# Live code
+# Guess the price / The price is right
+
+# 1- Greet the user
+# 2- Set the price of the product ( number to guess )
+# 3- Ask the user to guess and store it in a variable
+# 4- compare the users guess to the price to guess
+# 5- keep asking the to guess until they guess right
+# 6- Print final message when user guesses correctly
+
+puts 'Welcome The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+price_to_guess = rand(1..5)
+
+puts 'Guess a price between 1 and 5'
+user_guess = gets.chomp.to_i
+
+# while condition > while loop will keep iterating while the condition is true.
+# while !(user_guess == price_to_gess)
+# until user_guess == price_to_guess # opposite condition to while, until loop will keep iterating until the condition is true
+while user_guess != price_to_guess # the user guessed wrong
+  puts "Wrong guess, Try again!"
+  print '> '
+  user_guess = gets.chomp.to_i
+end
+
+puts "You guessed it right! The price to guess was #{price_to_guess}"
+
+# iterators => built in looping methods / function provided by ruby
+# to iterate over lists of element **and do something**
+
+numbers = (1..10).to_a
+
+for x in numbers
+  # puts "My number is #{x}"
+  # puts "print me!"
+end
+
+# .each
+numbers.each do |x|
+  puts "My number is #{x}"
+end
+
+# .each one liner
+numbers.each { |x| puts "My number is #{x}" } # only possible if the code is made out of one line ( only )
