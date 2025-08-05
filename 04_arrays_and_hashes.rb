485efe0f3b4336d3c7a5a4d43173bd5a23ee93e2
soufiane ezzine <ezzine.soufiane@gmail.com>
2025-08-06 03:07:26 +0900

    session 3 code + slides

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,135 @@
-
 ### Arrays
+[].class
+["str1", "str2"] # array of strings
+[1, 3, 4] # array of integers
+age = 32
+["soufiane", 214, true, [23, 34], age]
+[["soufiane", 34], ["carl", 30]]
+
+# Array
+# Arrays, also called lists, vectors, slices ... in other programming languages
+# They are a list of elements usually elements of the same datatype
+# Each element in an array can be identified by an index
+# Arrays in Ruby are zero indexed (indexing start at 0)
+
+students = ["Carl", "Julia", "Raphael", "Mark", "Mike"]
+# index        0       1        2         3
+# reverse idx  -4     -3        -2       -1
+
+
+# CRUD
+# Crud operations are the basic operations that can be applied to data
+# Originated from the database data manipulations
+array = [] # created an array
+
+# Create
+# Shovel operator << ( always adds at the end of the array)
+# Array << element
+students << "Chantal"
+
+# .push method
+# array.push(element) ( always adds at the end of the array)
+students.push("Charles")
+
+# .unshift() (add element at the beginning of the array)
+# array.unshift(element)
+students.unshift("Nathan")
+
+# Read
+# array[index]
+students[0]
+
+students[-1]
+students[-2]
+
+# array.first
+students.first
+# array.last
+students.last
+
+# array[range]
+students[0..2]
+students.first(2)
+
+# read the index of an element
+# array.index(element)
+students.index("Carl") # the string is case sensitive
+
+# Update
+# Array[index] = new_value
+students[0] = "Dan"
+students[-1] = "Frederico"
+
+
+# Delete
+# array.delete(element)
+students.delete("Carl")
+# array.delete_at(index)
+students.delete_at(-1)
+
+p students[10] # When trying to access an element that does not exist nil is returned
+p students.index("something") # nil
+
+# array methods
+
+students = ["Carl", "Julia", "Raphael", "Mark", "Mike"]
+
+# .each # loops over an array of element and does something
+# array.each do |element|
+#
+# end
+students.each do |x|
+  # puts "#{x} is a good student"
+end
+
+# students.each { |student| puts ... }
+
+# .each_with_index # Does the same thing as .each but gives access to the index as well
+students.each_with_index do |student, index|
+  # puts "#{index + 1}. #{student}"
+end
+
+# .map # creates a new array and transforms all the element in the array using the block logic
+# upcased = []
+# students.each do |student|
+#   upcased.push(student.upcase)
+# end
+#
+# p students
+# p upcased
+
+upcased = students.map do |student|
+  student.upcase
+end
+
+p students
+p upcased
+
+# .select # filters the array by removing all the element that do not return true to the blocks logic
+m_student = students.select do |student|
+  student[0] == "M"
+end
+
+p m_student
+
+# Block
+# everything between the do ... end
+# whatever is between the pipes is called a parameter ( like method definition )
+# it will alway return the last line of code ( just like a method )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,62 @@
 
 ### Methods
+# Also called functions, procedures ... in other programming langauges
+# Methods store a piece of code / some kind of logic, to be able to reuse it
+# Method is defined with the def key word closes with an end
+# Method takes a name ( the same ruby naming convention applies small_snake_case)
+# Methods may / or may not take parameters
+# The number of parameter need to match the number of arguments when the method is invoked / called
+# Method may or may not return a value
+# The return key word is optional ( in ruby )
+# The last line of a method will always be returned
+
+def add_two(number)
+  number + 2
+end
+
+number1 = add_two(4)
+number2 = add_two(5)
+number3 = add_two(7)
+
+p number1
+p number2
+p number3
+
+
+students = ["Carl", "Julia", "Raphael", "Mark"]
+# Define the method
+# Method define with parameters
+def display_names(team_array, team_name)
+  team_array.each do |member|
+    # puts "#{member} is a #{team_name} member!"
+  end
+end
+
+# Call / invoke / use the method
+# To use a method all I have to do is invoke its name
+# A methoid is called with arguments
+display_names(students, "The Coders")
+puts " "
+display_names(["Charles", "Nathan", "Carolina"], "The Players")
+
+# Naming convention
+# Methods that end with ?
+# Are methods that will (almost) always return a boolean
+12.even?
+1.odd?
+
+def is_even?(number)
+  number % 2 == 0
+end
+
+p is_even?(22)
+p is_even?(21)
+
+# Methods that end with !
+# Are Dangerous methods / Desctructive methods
+# They tranform the data data they have been called on
+name = "soufiane"
+p name.capitalize!
+p name # name variable content has been altered
+
+
