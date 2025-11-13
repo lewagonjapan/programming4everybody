fcb5432d30eac075ef9f2130eac8d9832caac9a2
soufiane ezzine <ezzine.soufiane@gmail.com>
2025-11-13 22:58:24 +0900

    add session 2 code

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,113 @@
 ### Controling Ruby Flow
+
+# conditionals
+
+# if condition
+#   # Code that runs if the condition is true
+# else
+#   # Code that runs if the condition is false
+# end
+
+my_age = 16
+legal_age = 18
+
+if my_age >= legal_age
+  puts "You are eligible to vote!"
+else
+  puts "You are not old enough to vote."
+end
+
+my_age = 16
+legal_age = 18
+
+is_eligible_to_vote = my_age >= legal_age
+
+# if !is_eligible_to_vote
+unless is_eligible_to_vote
+  puts "You are not old enough to vote."
+else
+  puts "You are eligible to vote!"
+end
+
+# Conditions
+# Any Ruby expression that returns a boolean
+2 + 3 # 5
+# Boolean expressions
+2 > 1 # greater than operator
+1 < 3 # less than operator
+1 == 1 # equality operator
+1 != 2 # not equal operator
+# !!!! Do not mistake this for the "=" assignment operator
+2 >= 3 # greater than or equal to operator
+3 <= 2 # less than or equal to operator
+
+# Keep in mind:
+# The order of conditions in a conditional statement is extremely important
+
+# Boolean expression combinations
+# Logical operators
+# Logical OR -> ||
+# For a combination of boolean expressions to be true using ||,
+# at least one of the expressions has to be true
+true || true # true
+false || true # true
+false || false # false
+
+# Logical AND -> &&
+# For a combination of boolean expressions to be true using &&,
+# all of the expressions have to evaluate to true
+true && true # evaluates to true
+false && false # false (because at least one of the expressions is false)
+
+# Logical NOT -> !
+# When put in front of a boolean expression, returns its opposite
+!true == false # true
+!false == true # true
+
+# Live code
+# Pseudo code:
+# 1. Greet the user
+# 2. Ask the user what time they are asking about
+# 3. Collect the user input and store it in a variable
+# 4. Compare the user input with the opening hours of our restaurant
+# 5. Print the correct information to the user
+
+puts "Welcome to Jiro Sushi 🍣"
+puts ''
+puts 'What time are you inquiring about?'
+print '> '
+
+time = gets.chomp.to_i
+
+if time == 11
+  puts "The restaurant is opening soon."
+elsif time > 19 && time <= 22
+  puts "The restaurant is open for dinner."
+elsif time > 12 && time <= 16
+  puts "The restaurant is open for lunch."
+else # default option
+  puts "The restaurant is closed."
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,84 @@
 ### Looping with Ruby
+# Range
+# A range is a datatype that represents an interval - a set of values with a beginning and an end
+p (1..10).class
+# .. means the last/end value will be included
+p (1..10).to_a
+# ... means the last/end value will be excluded
+p (1...10).to_a
+
+p ('a'...'z').to_a
+p ('A'...'Z').to_a
+p ('A'..'z').to_a # ASCII table
+
+# Each time the code repeats in a loop, we call that an iteration
+
+# For loop
+for x in (1..10)
+  puts "My number is #{x}"
+end
+
+names = ["carl", "amitesh", "johanna"]
+for name in names
+  puts "My name is #{name.capitalize}"
+end
+
+# Loop do is an infinite loop
+counter = 1
+
+loop do
+  puts "My number is #{counter}"
+  counter = counter + 1
+
+  if counter > 10
+    break
+  end
+end
+
+# .times
+10.times do
+  # puts "Print me"
+end
+
+# The while loop through live code
+# Pseudo code:
+
+# 1. Greet the user to our game
+# 2. Set a price for a product
+# 3. Ask the user to guess the price
+# 4. Keep asking until the user guesses the correct price
+# 5. Print a message when the user guesses correctly
+
+puts 'Welcome to The Price is Right! 🤑🤑🤑'
+puts '========'
+puts ''
+
+price_to_guess = rand(1..10)
+
+puts "Guess a price between 1 and 10:"
+print '> '
+user_guess = gets.chomp.to_i
+
+# until user_guess == price_to_guess
+while user_guess != price_to_guess
+  puts 'Try again:'
+  print '> '
+  user_guess = gets.chomp.to_i
+end
+
+puts "Correct! You guessed it right. The price was #{price_to_guess}."
+
+# Iterators
+names = ["carl", "amitesh", "johanna"]
+
+for name in names
+  puts "My name is #{name.capitalize}"
+end
+
+# .each method
+names.each do |name|
+  puts "My name is #{name.capitalize}"
+end
+
+# .each with one line
+names.each { |name| puts "My name is #{name.capitalize}" }
