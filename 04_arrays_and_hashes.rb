cd09b5d58d5c4ca7bf7380c98e5cf97936f55a91
soufiane ezzine <ezzine.soufiane@gmail.com>
2026-02-18 02:15:04 +0900

    session 3 code

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,94 @@
-
 ### Arrays
+# Also called lists, slices, vectors, etc., in other languages
+# Define an array using []
+
+[].class # Array
+[1, 2, 3] # Array of 3 integers
+[2.3, 4.5] # Array of 2 floats
+['yuki', 'kate', 'kim'] # Array of 3 strings
+[1, 'sylvain', true, [3.4, "string"], {}] # Array of mixed data types
+
+students = ['yuki', 'kate', 'kim', 'cyprien', 'emily']
+# index       0       1       2        3          4
+# reverse    -5      -4      -3       -2         -1
+
+# Basic operations on arrays: CRUD - Create, Read, Update, Delete
+
+# Create
+# Using [] or <<
+students << 'edsel'  # Shovel operator adds an element
+
+# Using push
+students.push("alain")
+p students
+
+# Read
+# Access by index
+p students[0]
+p students.first
+p students.first(2)
+
+p students.last
+p students[-1]
+
+# Find index of an element
+p students.index("kim")
+
+# Note: Accessing a non-existent index returns nil
+p students[7] # nil
+p students.index("soufiane") # nil
+
+# Update
+# Assign a new value to an index
+students[0] = "alain"
+students[-1] = "edsel"
+p students
+
+# Delete
+# Remove by value
+students.delete("alain")
+p students
+
+# Remove by index
+students.delete_at(0)
+p students
+
+# Iterators: Methods to loop through arrays
+# Iterator = the method used to iterate
+# Iterable = the data being looped over
+# The do...end is a block; method rules apply to blocks
+
+students = ['yuki', 'kate', 'kim', 'cyprien', 'emily']
+
+# .each method - Multiline iterator
+students.each do |student|
+  puts "Hello, #{student.capitalize}! Welcome to the class!"
+end
+
+# Single-line iterator
+students.each { |student| puts "Hello, #{student.capitalize}! Welcome to the class!" }
+
+# .each_with_index method
+students.each_with_index do |student, index|
+  puts "#{index + 1}. #{student.capitalize}"
+end
+
+# Build a new array with capitalized names using .each
+capitalized_students = []
+students.each do |student|
+  capitalized_students << student.capitalize
+end
+p capitalized_students
+
+# .map method - Creates a new array by transforming elements
+capitalized_students = students.map do |student|
+  student.capitalize
+end
+p capitalized_students
+
+# .count - Returns the array length or count matching a condition
+p students.count # Total length
+k_students = students.count do |student|
+  student.start_with?("k")
+end
+p k_students
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,60 @@
-
 ### Methods
+# Also called functions, methods, procedures, subroutines
+# To store a piece of logic / code that we want to reuse
+
+# Theory
+# We define a method using the def keyword
+# A method may or may not take parameters
+# We define a method with parameters (placeholders)
+# We call a method with arguments (actual values)
+# A method may or may not return a value
+# The return keyword is optional; in Ruby, a method returns the last line if not specified
+
+# Order of parameters vs. order of arguments
+# The order of arguments should match the order of parameters in the method definition
+# The datatype of the argument should match the datatype of the parameter in the method definition
+
+# Naming convention
+# - Methods are named using lower_snake_case
+# - Different ways to name a method:
+#     - Normal name: calculate_total
+#     - Question name: is_valid? (usually returns a boolean)
+#     - Bang name: save! (dangerous methods that change the data passed to them forever)
+
+# Problem definition
+puts "Hello, Aisha to the class"
+puts "Hello, Joshua to the class"
+puts "Hello, Thomas to the class"
+
+# Define my method
+def greet(student, class_name)
+  puts "Hello, #{student} to the #{class_name} class"
+end
+
+# Call the method / use the method
+greet("Aisha", "ruby")
+# greet("databases", "Joshua") # No good
+greet("Thomas", "ruby")
+
+def calculate_total(pizza_price, num_pizzas)
+  pizza_price * num_pizzas
+end
+
+total_cost = calculate_total(3, 10)
+
+puts "You owe us #{total_cost} dollars"
+
+def is_even?(number)
+  number % 2 == 0
+end
+
+puts is_even?(5) # false
+puts is_even?(6) # true
+
+name = "thomas"
+
+puts name.upcase
+puts name
+
+puts name.upcase!
+puts name
