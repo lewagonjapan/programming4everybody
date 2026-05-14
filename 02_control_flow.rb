7744e42368d54c27ca14f394e0e602bf9b7eb033
soufiane ezzine <ezzine.soufiane@gmail.com>
2026-05-14 22:26:53 +0900

    session 2 code

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,113 @@
-### Controling Ruby Flow
+### Controlling Ruby Flow
+
+# if condition
+#   # code to execute if the condition is true
+# else
+#   # code to execute if the condition is false
+# end
+
+age = 20
+if age < 18
+  puts "You can't vote yet! Come back later."
+else
+  puts "You can vote!"
+end
+# Note: `if !(age >= 18)` also works, but `if age < 18` is clearer.
+# Whenever you find yourself negating with `!`, it's usually easier
+# to flip the comparison instead.
+
+
+# condition  => a Ruby expression that evaluates to true or false
+# expression => any Ruby code that evaluates to a value
+
+# arithmetic expressions
+2 + 2 # => 4
+5 * 3 # => 15
+
+# assignment expressions
+age = 20          # => 20  (assignment also returns the value assigned)
+new_age = age + 1 # => 21
+
+# boolean expressions => evaluate to true or false
+age >= 18 # greater than or equal to
+age > 18  # greater than
+age <= 18 # less than or equal to
+age < 18  # less than
+13 == 13  # equality   => true
+14 != 13  # not equal  => true
+# /!\ Don't confuse `=` (assignment) with `==` (equality check):
+#     age = 18   sets age to 18
+#     age == 18  asks whether age equals 18
+
+# Logical operators
+
+# Logical AND => &&
+# (age >= 18) && (city == "shinagawa")
+# For the whole expression to be true, ALL conditions must be true.
+# true  && true  && true  => true
+# true  && false && true  => false
+
+# Logical OR => ||
+# (city == "meguro") || (city == "shinagawa")
+# For the whole expression to be true, AT LEAST ONE condition must be true.
+# false || false || true  => true
+# false || false || false => false
+
+# Logical NOT => !
+# !true  => false
+# !false => true
+
+
+# livecode
+# Pseudo code
+# 1- Welcome the user to our application
+# 2- Ask the user what time they are inquiring about
+# 3- Get the user's answer and store it in a variable
+# 4- Compare the user input to the opening hours of the restaurant
+# 5- Print the correct message to the user
+
+puts "Welcome to Jiro Sushi 🍣"
+puts ''
+puts 'What time are you inquiring about?'
+print '> '
+
+time = gets.chomp.to_i
+
+if time == 11
+  puts "The restaurant is opening soon!"
+elsif time >= 12 && time < 15
+  puts "The restaurant is open for lunch!"
+elsif time >= 19 && time < 22
+  puts "The restaurant is open for dinner!"
+else
+  puts "The restaurant is closed!"
+end
+
+
+# Ruby truthiness
+# In Ruby, ALL values are considered "truthy" except `nil` and `false`.
+# That means 0, "", [], and {} are all truthy — unlike in many other languages.
+
+# var = "some string"
+#
+# if var
+#   puts "My variable is truthy"
+# else
+#   puts "My variable is falsy"
+# end
+
+
+# unless => the opposite of if. Runs the block when the condition is FALSE.
+#
+# unless condition
+#   # code to execute if the condition is FALSE
+# else
+#   # code to execute if the condition is TRUE
+# end
+#
+# Example:
+# unless age >= 18
+#   puts "You can't vote yet!"
+# else
+#   puts "You can vote!"
+# end
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,83 @@
 ### Looping with Ruby
+
+# Instead of writing this 10 times...
+# puts "My number is 1"
+# puts "My number is 2"
+# puts "My number is 3"
+# ...
+# puts "My number is 10"
+# ...we use a loop!
+
+
+# x.times => repeats a block x times
+# The block variable starts at 0, so we add 1 to display 1..10
+10.times do |int|
+  # puts "My number is #{int + 1}!"
+end
+
+
+# New data type: Range  yaay
+# (1..10).class # => Range
+1..10    # => range from 1 to 10 (10 INCLUDED)
+1...10   # => range from 1 to 9  (10 EXCLUDED — note the three dots)
+'a'..'z' # => a, b, c, ... x, y, z
+'A'..'Z' # => A, B, C, ... X, Y, Z
+# A Range is NOT an array. If you need an array, call .to_a:
+# (1..10).to_a # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# for loop
+for int in 1..10
+  # puts "My number is #{int}!"
+end
+# Note: in Ruby, `for` is rarely used. `.each` and `.times` are more idiomatic.
+
+
+# loop do => infinite loop, exits with `break`
+my_number = 1
+loop do
+  puts "My number is #{my_number}"
+  my_number += 1   # shorthand for: my_number = my_number + 1
+
+  break if my_number > 10   # Ruby idiom: trailing `if` reads like English
+end
+
+
+# live code
+# 1- Greet the user to our super awesome game
+# 2- Decide on the price to guess and store it in a variable
+# 3- Ask the user to guess the price (between 1 and 5)
+# 4- Store the user's guess in a variable
+# 5- Keep asking the user until they guess right
+# 6- Print the congratulations message
+
+puts "Welcome to the price guessing game!"
+puts "=============================="
+
+price_to_guess = rand(1..5)   # random integer between 1 and 5 (inclusive)
+
+puts "Guess a price between 1 and 5"
+print '> '
+
+user_guess = gets.chomp.to_i
+
+# while condition => keep looping AS LONG AS the condition is true
+while user_guess != price_to_guess
+  puts "Wrong! Guess again between 1 and 5"
+  print '> '
+  user_guess = gets.chomp.to_i
+end
+
+puts "Congratulations! You guessed the price right! #{price_to_guess} is the correct price!"
+
+
+# .each loop => the most common way to iterate in Ruby
+# Works on any collection (Range, Array, Hash...)
+
+
+# Quick recap of loop choices:
+# - x.times    => when you just need to repeat N times
+# - .each      => when you have a collection to go through (most common!)
+# - while      => when you don't know how many times you'll loop (e.g. user input)
+# - loop do    => infinite loop, you control the exit with `break`
+# - for ... in => exists, but Rubyists usually prefer .each
