20d28fb7329e9aa344a009001c930ef4240c0da3
soufiane ezzine <ezzine.soufiane@gmail.com>
2026-08-20 22:13:16 +0900

    session 2 code and slides

--- 02_control_flow.rb
+++ 02_control_flow.rb
@@ -1,1 +1,87 @@
-### Controling Ruby Flow
+### Controlling Ruby Flow
+# This file is used to live-code each part during the live session
+
+# Conditionals
+# if condition
+#   # this code runs if the condition is true
+# else
+#   # this code runs otherwise
+# end
+
+time = 15
+
+if time > 11
+  # puts "My restaurant is open!"
+else
+  # puts "My restaurant is closed!"
+end
+
+# A condition is an expression that returns a boolean (true or false)
+
+# Arithmetic expressions
+1 + 2
+1 / 3
+2 * 5
+# Variable expression
+age = 32
+# Boolean expressions
+15 > 11 # Greater than
+14 >= 14 # Greater than or equal to
+12 < 11 # Less than
+12 <= 12 # Less than or equal to
+11 == 11 # Equality operator
+11 != 12 # Not equal
+# DO NOT CONFUSE == WITH THE = (ASSIGNMENT) OPERATOR
+# BE CAREFUL WHEN COMPARING VALUES: THEY SHOULD BE THE SAME DATA TYPE
+
+# Ruby is a "truthy" language:
+# everything is considered true, except for false and nil
+
+# Logical AND (&&)
+# For a logical AND expression to be true, BOTH sides must be true
+# true && true  ==> true
+# true && false ==> false
+
+# Logical OR (||)
+# For a logical OR expression to be true, AT LEAST ONE side must be true
+# true || true  ==> true
+# true || false ==> true
+
+# Pseudocode
+# A program to inform users about the opening hours of my restaurant
+
+# 1. Welcome the user to our app / restaurant
+# 2. Ask the user for the time they are inquiring about
+# 3. Read the user's input and store it in a variable
+# 4. Compare the value in that variable to the restaurant's opening hours
+# 5. Print the correct message to the user
+
+puts 'Welcome to Jiro Sushi 🍣'
+puts ''
+puts 'What time are you inquiring about?'
+print '> '
+
+time = gets.chomp.to_i
+
+if time == 11
+  puts "The restuarant is opening soon!"
+elsif time >= 19 && time < 23
+  puts "The restaurant is open for dinner"
+elsif time >= 12 && time < 16
+  puts "My restaurant is open for lunch"
+else
+  puts "My restaurant is closed!"
+end
+
+
+# Logical NOT operator (!)
+# true == !false
+# true == true
+
+# age = 34
+# # if !(age > 18)
+# unless age > 18
+#   puts "you can not vote sorry"
+# else
+#   puts "you can vote!"
+# end
--- 03_looping.rb
+++ 03_looping.rb
@@ -1,1 +1,89 @@
 ### Looping with Ruby
+#
+# DRY: Don't Repeat Yourself
+
+# Range data type
+# Represents an interval between two values, e.g. 1..100
+(1..10).class # Range
+(1..100).to_a
+# .. (two dots): the end value is included
+# ... (three dots): the end value is excluded
+('a'..'z').to_a
+('A'..'Z').to_a
+# ('A'..'z').to_a
+
+# For loop
+for number in (1..10).to_a
+  # puts "#{number} - print me!"
+end
+
+
+# loop do
+# counter = 1
+#
+# loop do
+#   puts "my number is #{counter}"
+#   counter = counter + 1
+#
+#   if counter > 10
+#     break
+#   end
+# end
+
+# .times
+10.times do
+  # puts "Print me!"
+end
+
+
+# Live code
+# The Price is Right game
+
+# 1. Greet the user
+# 2. Set the price of the product
+# 3. Ask the user to guess the price (store it in a variable)
+# 4. Keep asking until the user guesses correctly
+# 5. Print a final message when the user guesses correctly
+
+puts 'Welcome The Price is Right!! 🤑🤑🤑'
+puts '========'
+puts ''
+
+price_to_guess = rand(1..5)
+
+puts 'Guess a price between 1 and 5'
+user_guess = gets.chomp.to_i
+
+# while loop
+
+# if price_to_guess == user_guess
+#   puts "You guessed it right!"
+# else
+#   puts "you guessed it wrong"
+# end
+
+while user_guess != price_to_guess
+  puts 'Wrong, try again!'
+  print '> '
+  user_guess = gets.chomp.to_i
+end
+
+puts "You guess right the price to guess was #{price_to_guess}!"
+
+
+# Iterators in Ruby
+students = ['Lance', 'Emily', 'Kim']
+# for name in students
+#   puts "Hello #{name}, welcome"
+# end
+
+# .each
+# array.each do |name|
+
+students.each do |name|
+  puts "Hello #{name}, welcome"
+  puts " something else "
+end
+
+# One-liner
+students.each { |name| puts "Hello #{name}, welcome" }
