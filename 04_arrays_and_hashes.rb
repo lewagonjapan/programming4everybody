2fb253b54f296626a4dc222fa200435e54b2bb10
soufiane ezzine <ezzine.soufiane@gmail.com>
2026-08-26 11:23:52 +0900

    session 3 code and slides

--- 04_arrays_and_hashes.rb
+++ 04_arrays_and_hashes.rb
@@ -1,2 +1,128 @@
-
 ### Arrays
+
+# Recap: Arrays
+[].class
+[1, 2, 4]
+["string1", "string2", "string3"]
+
+students = ["Simon", "Lance", "Vu", "Prabesh", "Emily"]
+# index         0        1       2        3        4
+# reverse idx  -5       -4      -3       -2       -1
+
+# CRUD
+# Create
+# Read
+# Update
+# Delete
+
+# Create (adding)
+new_array = []
+
+# shovel operator <<
+# array << new_element
+# students << "Yohei"
+
+# push method (.push)
+# array.push(element)
+# students.push("David")
+# p students
+
+# Read
+# array[index]
+p students[0]
+
+# .first
+p students.first
+p students[4]
+
+p students[-1]
+p students.last
+
+# use a range to get a slice of the array
+# p students[0..2]
+
+# .index(element)
+p students.index("Yohei")
+
+p students[10]
+p students.index("Soufiane")
+
+# Update
+# array[index] = new_value
+students[2] = "Helen"
+
+# Delete
+# array.delete(element)
+# students.delete("Helen")
+
+# array.delete_at(index)
+# students.delete_at(2)
+p students
+
+
+students = ["Simon", "Lance", "Lionel", "Helen", "Prabesh", "Emily"]
+
+# Iterators
+# array.iterator do |element|
+#   repeat something using the element
+# end
+
+# .each
+students.each do |name|
+  puts "Hello, #{name} welcome to today's course!"
+end
+
+# one-liner
+students.each { |name| puts "Hello, #{name} welcome to today's course!" }
+
+
+prices = [2, 6, 4, 5, 4]
+
+with_tax = []
+prices.each do |price|
+  with_tax << price * 1.08
+end
+
+p with_tax
+
+# .map
+with_tax = prices.map do |price|
+  price * 1.08
+end
+
+p with_tax
+
+# .sum
+total = with_tax.sum
+p total
+
+# .select
+l_students = students.select do |student|
+  student[0] == "L"
+end
+
+p l_students
+
+
+# Pseudo code
+# Disclaimer: this is a bit advanced.
+
+# A scraper to collect prices from Rakuten:
+# 1. be able to open a web page using Ruby
+# 2. understand the HTML structure
+# 3. search for the elements that represent a price
+# 4. extract those elements
+
+# import a module / package / gem to open websites
+require "open-uri"
+
+# import a module / package / gem to parse HTML
+require "nokogiri"
+
+html = URI.open("https://search.rakuten.co.jp/search/mall/pillow").read
+
+parsed_html = Nokogiri::HTML(html)
+
+parsed_html.search(".price--3zUvK").each do |price|
+  puts price.text.strip
+end
--- 05_methods_and_blocks.rb
+++ 05_methods_and_blocks.rb
@@ -1,2 +1,69 @@
-
 ### Methods
+
+# Define methods
+# Functions, procedures, methods
+# Reuse logic: define it once and reuse it multiple times.
+
+# Theory
+# - a method is defined using `def ... end`
+# - the naming convention of a method is the same as a variable: lower_snake_case
+# - a method may or may not take parameters
+# - a method may or may not return a value
+# - if a method returns data, the `return` keyword is optional
+# - a method will always return its last line
+
+# puts "Hello, Lance welcome to today's course!"
+# puts "Hello, Yohei welcome to today's course!"
+# puts "Hello, Emily welcome to today's course!"
+
+# Define a method / function
+# Define a method with parameters
+def greet(name)
+  puts "Hello, #{name} welcome to today's course!"
+end
+
+# invoke / call / use the method
+# Call a method with arguments
+greet("Lance")
+greet("Yohei")
+greet("Emily")
+
+# calculate_total calculates the total price of an order
+# takes pizza_price (integer)
+# takes num_pizza (integer)
+# returns an integer
+def calculate_total(pizza_price, num_pizza)
+  total = (pizza_price - 2) * num_pizza
+  return total
+end
+
+# calculate_total(11.23, 5)
+
+receipt1 = calculate_total(12.33, 2)
+receipt2 = calculate_total(2, 12.33) # argument order matters - this is not the same
+puts receipt1
+puts receipt2
+
+
+# Methods defined with ?
+# Should return booleans
+13.odd?
+13.even?
+
+def is_even?(number)
+  number % 2 == 0
+end
+
+p is_even?(13)
+p is_even?(10)
+
+
+# Methods defined with !
+# DANGEROUS / MUTATING METHODS
+name = "yohei"
+name.upcase!
+puts name
+
+
+# Blocks follow the same rules as methods.
+# You can think of them as methods without a name.
